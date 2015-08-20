fea54b0b6c8057e99f27b2278da98c64caf3eccb
Danni Yu <dyu@thoughtworks.com>
2015-08-20 19:57:22 +0800

    oops, still wrong url... sorry its late im not using my brain any more :( :sunflower:

--- contractTests/spec/spec_helper.rb
+++ contractTests/spec/spec_helper.rb
@@ -3,7 +3,7 @@ require 'rest-client'
 require 'json'
 require 'base64'
 
-WEB_UAT_URI = "52.69.124.32:8080"
+WEB_UAT_URI = "52.69.106.103:8888"
 
 def http_basic_auth(username, password)
   auth = Base64.strict_encode64("#{username}:#{password}")
