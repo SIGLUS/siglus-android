01dee079384db55fbee357ad0655b5223b683b1f
Yingli Yuan <ylyuan@thoughtworks.com>
2015-08-24 13:52:13 +0800

    YL - #000 - hardcode mmia program code

--- app/src/main/java/org/openlmis/core/model/repository/MMIARepository.java
+++ app/src/main/java/org/openlmis/core/model/repository/MMIARepository.java
@@ -42,7 +42,7 @@ public class MMIARepository extends RnrFormRepository {
     public static final String ATTR_PTV = "PTV";
     public static final String ATTR_PPE = "PPE";
 
-    public static final String MMIA_PROGRAM_CODE = "ESS_MEDS";
+    public static final String MMIA_PROGRAM_CODE = "VACCINES";
 
     @Inject
     ProgramRepository programRepository;
