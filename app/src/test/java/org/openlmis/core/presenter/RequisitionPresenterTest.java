d9d5a4913d4e37c0b245fb1d934d9be14b7d5347
Yingli Yuan <ylyuan@thoughtworks.com>
2015-08-26 16:07:58 +0800

    ZS&YL - #73 - add test for isCompleted in requisition presenter

---
+++ app/src/test/java/org/openlmis/core/presenter/RequisitionPresenterTest.java
@@ -0,0 +1,99 @@
+/*
+ * This program is part of the OpenLMIS logistics management information
+ * system platform software.
+ *
+ * Copyright © 2015 ThoughtWorks, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version. This program is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details. You should
+ * have received a copy of the GNU Affero General Public License along with
+ * this program. If not, see http://www.gnu.org/licenses. For additional
+ * information contact info@OpenLMIS.org
+ */
+
+
+package org.openlmis.core.presenter;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openlmis.core.LMISTestRunner;
+import org.openlmis.core.exceptions.ViewNotMatchException;
+import org.openlmis.core.model.Product;
+import org.openlmis.core.model.Program;
+import org.openlmis.core.model.RnrFormItem;
+import org.openlmis.core.view.activity.RequisitionActivity;
+import org.openlmis.core.view.viewmodel.RequisitionFormItemViewModel;
+import org.robolectric.Robolectric;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import roboguice.RoboGuice;
+
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertTrue;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+@RunWith(LMISTestRunner.class)
+public class RequisitionPresenterTest {
+
+    private RequisitionPresenter presenter;
+    private RequisitionActivity mockActivity;
+
+    @Before
+    public void setup() throws ViewNotMatchException {
+        mockActivity = mock(RequisitionActivity.class);
+
+        presenter = RoboGuice.getInjector(Robolectric.application).getInstance(RequisitionPresenter.class);
+        presenter.attachView(mockActivity);
+    }
+
+    @Test
+    public void shouldReturnFalseWhenRequestAmountIsNull() throws Exception {
+
+        List<RequisitionFormItemViewModel> list = new ArrayList<RequisitionFormItemViewModel>();
+        for (int i = 0; i < 3; i++) {
+            list.add(new RequisitionFormItemViewModel(createRnrFormItem(i)));
+        }
+
+        presenter.requisitionFormItemViewModelList = list;
+        assertFalse(presenter.isCompleted());
+        verify(mockActivity).showInputError(anyInt());
+    }
+
+    @Test
+    public void shouldReturnTrueWhenRequestAmountIsNotNull() throws Exception {
+
+        List<RequisitionFormItemViewModel> list = new ArrayList<RequisitionFormItemViewModel>();
+        for (int i = 0; i < 3; i++) {
+            RequisitionFormItemViewModel requisitionFormItemViewModel = new RequisitionFormItemViewModel(createRnrFormItem(i));
+            requisitionFormItemViewModel.setRequestAmount("12");
+            list.add(requisitionFormItemViewModel);
+        }
+
+        presenter.requisitionFormItemViewModelList = list;
+        assertTrue(presenter.isCompleted());
+    }
+
+    private RnrFormItem createRnrFormItem(int i) {
+        Program program = new Program();
+        program.setProgramCode("1");
+        Product product = new Product();
+        product.setProgram(program);
+        product.setId(1);
+        RnrFormItem rnrFormItem = new RnrFormItem();
+        rnrFormItem.setInventory(1000);
+        rnrFormItem.setIssued(i);
+        rnrFormItem.setProduct(product);
+        return rnrFormItem;
+    }
+
+}
