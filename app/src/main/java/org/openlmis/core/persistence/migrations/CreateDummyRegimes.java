bb13f280c5eeaccb09dece2cec9579f99b482e5a
Yingli Yuan <ylyuan@thoughtworks.com>
2015-08-28 11:46:41 +0800

    ZS & YL -#000- hard code regime code

--- app/src/main/java/org/openlmis/core/persistence/migrations/CreateDummyRegimes.java
+++ app/src/main/java/org/openlmis/core/persistence/migrations/CreateDummyRegimes.java
@@ -46,25 +46,25 @@ public class CreateDummyRegimes implements Migration {
                 @Override
                 public String operate(Dao dao) throws SQLException {
 
-                    createRegime(dao, "AZT+3TC+NVP", Regimen.RegimeType.ADULT);
-                    createRegime(dao, "TDF+3TC+EFV", Regimen.RegimeType.ADULT);
-                    createRegime(dao, "AZT+3TC+EFV", Regimen.RegimeType.ADULT);
-                    createRegime(dao, "d4T 30+3TC+NVP", Regimen.RegimeType.ADULT);
-                    createRegime(dao, "d4T 30+3TC+EFV", Regimen.RegimeType.ADULT);
-                    createRegime(dao, "AZT+3TC+LPV/r", Regimen.RegimeType.ADULT);
-                    createRegime(dao, "TDF+3TC+LPV/r", Regimen.RegimeType.ADULT);
-                    createRegime(dao, "ABC+3TC+LPV/r", Regimen.RegimeType.ADULT);
+                    createRegime(dao, "001","AZT+3TC+NVP", Regimen.RegimeType.ADULT);
+                    createRegime(dao, "002","TDF+3TC+EFV", Regimen.RegimeType.ADULT);
+                    createRegime(dao, "003","AZT+3TC+EFV", Regimen.RegimeType.ADULT);
+                    createRegime(dao, "004","d4T 30+3TC+NVP", Regimen.RegimeType.ADULT);
+                    createRegime(dao, "005","d4T 30+3TC+EFV", Regimen.RegimeType.ADULT);
+                    createRegime(dao, "006","AZT+3TC+LPV/r", Regimen.RegimeType.ADULT);
+                    createRegime(dao, "007","TDF+3TC+LPV/r", Regimen.RegimeType.ADULT);
+                    createRegime(dao, "008","ABC+3TC+LPV/r", Regimen.RegimeType.ADULT);
 
-                    createRegime(dao, "d4T+3TC+NVP(3DFC Baby)", Regimen.RegimeType.BABY);
-                    createRegime(dao, "d4T+3TC+LPV/r(2DFC Baby + LPV/r)", Regimen.RegimeType.BABY);
-                    createRegime(dao, "d4T+3TC+ABC(2DFC Baby + ABC)", Regimen.RegimeType.BABY);
-                    createRegime(dao, "d4T+3TC+EFV(2DFC Baby + EFV)", Regimen.RegimeType.BABY);
-                    createRegime(dao, "AZT60+3TC+NVP(3DFC)", Regimen.RegimeType.BABY);
-                    createRegime(dao, "AZT60+3TC+EFV(2DFC + EFV)", Regimen.RegimeType.BABY);
-                    createRegime(dao, "AZT60+3TC+ABC(2DFC + ABC)", Regimen.RegimeType.BABY);
-                    createRegime(dao, "AZT60+3TC+LPV/r(2DFC + LPV/r)", Regimen.RegimeType.BABY);
-                    createRegime(dao, "ABC+3TC+LPV/r", Regimen.RegimeType.BABY);
-                    createRegime(dao, "ABC+3TC+EFZ", Regimen.RegimeType.BABY);
+                    createRegime(dao, "009","d4T+3TC+NVP(3DFC Baby)", Regimen.RegimeType.BABY);
+                    createRegime(dao, "010","d4T+3TC+LPV/r(2DFC Baby + LPV/r)", Regimen.RegimeType.BABY);
+                    createRegime(dao, "011","d4T+3TC+ABC(2DFC Baby + ABC)", Regimen.RegimeType.BABY);
+                    createRegime(dao, "012","d4T+3TC+EFV(2DFC Baby + EFV)", Regimen.RegimeType.BABY);
+                    createRegime(dao, "013","AZT60+3TC+NVP(3DFC)", Regimen.RegimeType.BABY);
+                    createRegime(dao, "014","AZT60+3TC+EFV(2DFC + EFV)", Regimen.RegimeType.BABY);
+                    createRegime(dao, "015","AZT60+3TC+ABC(2DFC + ABC)", Regimen.RegimeType.BABY);
+                    createRegime(dao, "016","AZT60+3TC+LPV/r(2DFC + LPV/r)", Regimen.RegimeType.BABY);
+                    createRegime(dao, "017","ABC+3TC+LPV/r", Regimen.RegimeType.BABY);
+                    createRegime(dao, "018","ABC+3TC+EFZ", Regimen.RegimeType.BABY);
 
                     return null;
                 }
@@ -74,8 +74,9 @@ public class CreateDummyRegimes implements Migration {
         }
     }
 
-    private void createRegime(Dao dao, String name, Regimen.RegimeType regimeType) throws SQLException {
+    private void createRegime(Dao dao, String code ,String name, Regimen.RegimeType regimeType) throws SQLException {
         Regimen regimen = new Regimen();
+        regimen.setCode(code);
         regimen.setName(name);
         regimen.setType(regimeType);
         dao.create(regimen);
