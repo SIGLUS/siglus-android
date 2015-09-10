d055e8f884bc740c2eac5b659b84f666745453f7
Danni Yu <dyu@thoughtworks.com>
2015-09-10 17:59:37 +0800

    use new CI url for contract tests

--- contractTests/spec/spec_helper.rb
+++ contractTests/spec/spec_helper.rb
@@ -3,7 +3,7 @@ require 'rest-client'
 require 'json'
 require 'base64'
 
-WEB_DEV_URI = "52.69.124.32:8080"
+WEB_DEV_URI = "52.69.16.156:8000"
 
 def http_basic_auth(username, password)
   auth = Base64.strict_encode64("#{username}:#{password}")
