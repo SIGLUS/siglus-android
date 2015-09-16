a4eaeccdef836dbb759b57f87e8c7df45231767c
sun zhuo <zsun@thoughtworks.com>
2015-09-16 09:47:09 +0800

    ZS -#000- fix hide keyboard bug

--- functionalTests/features/step_definitions/login_steps.rb
+++ functionalTests/features/step_definitions/login_steps.rb
@@ -2,8 +2,10 @@ require 'calabash-android/calabash_steps'
 
 When /^I enter username "([^\"]+)"$/ do |username|
   enter_text("android.widget.EditText id:'tx_username'", username)
+  hide_soft_keyboard
 end
 
 When /^I enter password "([^\"]+)"$/ do |password|
   enter_text("android.widget.EditText id:'tx_password'", password)
+  hide_soft_keyboard
 end
