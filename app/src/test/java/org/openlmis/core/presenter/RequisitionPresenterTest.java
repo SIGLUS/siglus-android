a8dbcc784b0f2ab965fb3c27ad3b5663f6ef69f6
sun zhuo <zsun@thoughtworks.com>
2015-09-21 17:39:09 +0800

    ZS&YL -#76- add Test for loadForm

--- app/src/test/java/org/openlmis/core/presenter/RequisitionPresenterTest.java
+++ app/src/test/java/org/openlmis/core/presenter/RequisitionPresenterTest.java
@@ -19,18 +19,25 @@
 
 package org.openlmis.core.presenter;
 
+import com.google.inject.AbstractModule;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.MockitoAnnotations;
 import org.openlmis.core.LMISTestRunner;
+import org.openlmis.core.exceptions.LMISException;
 import org.openlmis.core.exceptions.ViewNotMatchException;
 import org.openlmis.core.model.Product;
 import org.openlmis.core.model.Program;
+import org.openlmis.core.model.RnRForm;
 import org.openlmis.core.model.RnrFormItem;
+import org.openlmis.core.model.repository.VIARepository;
 import org.openlmis.core.view.activity.RequisitionActivity;
 import org.openlmis.core.view.viewmodel.RequisitionFormItemViewModel;
 import org.robolectric.RuntimeEnvironment;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -40,17 +47,24 @@ import static junit.framework.Assert.assertFalse;
 import static junit.framework.Assert.assertTrue;
 import static org.mockito.Matchers.anyInt;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @RunWith(LMISTestRunner.class)
 public class RequisitionPresenterTest {
 
     private RequisitionPresenter presenter;
     private RequisitionActivity mockActivity;
+    private VIARepository viaRepository;
 
     @Before
     public void setup() throws ViewNotMatchException {
+        viaRepository = mock(VIARepository.class);
         mockActivity = mock(RequisitionActivity.class);
+
+        RoboGuice.overrideApplicationInjector(RuntimeEnvironment.application, new MyTestModule());
+        MockitoAnnotations.initMocks(this);
 
         presenter = RoboGuice.getInjector(RuntimeEnvironment.application).getInstance(RequisitionPresenter.class);
         presenter.attachView(mockActivity);
@@ -59,7 +73,7 @@ public class RequisitionPresenterTest {
     @Test
     public void shouldReturnFalseWhenRequestAmountIsNull() throws Exception {
 
-        List<RequisitionFormItemViewModel> list = new ArrayList<RequisitionFormItemViewModel>();
+        List<RequisitionFormItemViewModel> list = new ArrayList<>();
         for (int i = 0; i < 3; i++) {
             list.add(new RequisitionFormItemViewModel(createRnrFormItem(i)));
             list.get(i).setRequestAmount("");
@@ -73,7 +87,7 @@ public class RequisitionPresenterTest {
     @Test
     public void shouldReturnTrueWhenRequestAmountIsNotNull() throws Exception {
 
-        List<RequisitionFormItemViewModel> list = new ArrayList<RequisitionFormItemViewModel>();
+        List<RequisitionFormItemViewModel> list = new ArrayList<>();
         for (int i = 0; i < 3; i++) {
             RequisitionFormItemViewModel requisitionFormItemViewModel = new RequisitionFormItemViewModel(createRnrFormItem(i));
             requisitionFormItemViewModel.setRequestAmount("12");
@@ -82,6 +96,29 @@ public class RequisitionPresenterTest {
 
         presenter.requisitionFormItemViewModelList = list;
         assertTrue(presenter.isRequisitionFormAmountCompleted());
+    }
+
+    @Test
+    public void shouldGetRnRFormById() throws Exception {
+        presenter.loadRnrForm(1);
+        verify(viaRepository).queryRnRForm(anyInt());
+        verify(viaRepository, never()).getDraftVIA();
+    }
+
+    @Test
+    public void shouldGetInitForm() throws LMISException, SQLException {
+        when(viaRepository.getDraftVIA()).thenReturn(null);
+        presenter.loadRnrForm(0);
+        verify(viaRepository).getDraftVIA();
+        verify(viaRepository).initVIA();
+    }
+
+    @Test
+    public void shouldGetDraftForm() throws LMISException {
+        when(viaRepository.getDraftVIA()).thenReturn(new RnRForm());
+        presenter.loadRnrForm(0);
+        verify(viaRepository).getDraftVIA();
+        verify(viaRepository, never()).initVIA();
     }
 
     private RnrFormItem createRnrFormItem(int i) {
@@ -95,6 +132,13 @@ public class RequisitionPresenterTest {
         rnrFormItem.setIssued(i);
         rnrFormItem.setProduct(product);
         return rnrFormItem;
+    }
+
+    public class MyTestModule extends AbstractModule {
+        @Override
+        protected void configure() {
+            bind(VIARepository.class).toInstance(viaRepository);
+        }
     }
 
 }
