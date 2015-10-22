a0c40b33678721278fadc356a5313115ba85df70
sun zhuo <zsun@thoughtworks.com>
2015-10-22 10:14:16 +0800

    ZS -#000- fix Functional Test  Bug

--- functionalTests/features/step_definitions/env_config.rb
+++ functionalTests/features/step_definitions/env_config.rb
@@ -8,15 +8,11 @@ module EnvConfig
             {
                 username:"superuser",
                 password:"password1",
-                stockcard_name_MMIA:"Lamivudina 150mg/Zidovudina 300mg/Nevirapina 200mg [08S42]"
-                product_name_MMIA:"Lamivudina 150mg/Zidovudina 300mg/Nevirapina 200mg"
             }
         else
             {
                 username:"test_user",
                 password:"testuser",
-                stockcard_name_MMIA:"Lamivudina 150mg/Zidovudina 300mg/Nevirapina 200mg [08S42]"
-                product_name_MMIA:"Lamivudina 150mg/Zidovudina 300mg/Nevirapina 200mg"
             }
         end
     end
