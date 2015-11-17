f11253ff17f5de0ee7828bf3a2db1599dde9051b
sun zhuo <zsun@thoughtworks.com>
2015-11-17 13:36:59 +0800

    ZS&ZY -#000- refactor code

--- app/src/main/java/org/openlmis/core/view/fragment/SimpleDialogFragment.java
+++ app/src/main/java/org/openlmis/core/view/fragment/SimpleDialogFragment.java
@@ -20,12 +20,14 @@ package org.openlmis.core.view.fragment;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
-import android.app.DialogFragment;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.text.TextUtils;
 
-public class SimpleDialogFragment extends DialogFragment {
+import roboguice.fragment.provided.RoboDialogFragment;
+
+
+public class SimpleDialogFragment extends RoboDialogFragment {
 
     private static final String ARG_TITLE = "title";
     private static final String ARG_MESSAGE = "message";
