e0ce5c446ebdb119fe37feebd5af40864d40af42
cuipengfei <cuipengfei2008@gmail.com>
2015-11-26 15:42:19 +0800

    CPF - #000 - extract method to simplify form period test

--- app/src/test/java/org/openlmis/core/model/RnRFormTest.java
+++ app/src/test/java/org/openlmis/core/model/RnRFormTest.java
@@ -1,42 +1,39 @@
 package org.openlmis.core.model;
 
-import org.junit.Before;
 import org.junit.Test;
-import org.openlmis.core.utils.DateUtil;
 
 import java.util.Date;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.openlmis.core.utils.DateUtil.SIMPLE_DATE_FORMAT;
+import static org.openlmis.core.utils.DateUtil.parseString;
 
 public class RnRFormTest {
-    @Before
-    public void setUp() {
-    }
 
     @Test
     public void shouldGetPeriodByPeriodBegin() throws Exception {
-        RnRForm rnRForm=new RnRForm();
-        Date periodBegin = DateUtil.parseString("21/09/2015", DateUtil.SIMPLE_DATE_FORMAT);
-        rnRForm.setPeriodBegin(periodBegin);
-        RnRForm.setPeriodByPeriodBegin(rnRForm);
+        Date periodBegin = parseString("21/09/2015", SIMPLE_DATE_FORMAT);
+        Date periodEnd = parseString("20/10/2015", SIMPLE_DATE_FORMAT);
 
-        assertThat(rnRForm.getPeriodBegin(), is(periodBegin));
-        Date periodEnd = DateUtil.parseString("20/10/2015", DateUtil.SIMPLE_DATE_FORMAT);
-        assertThat(rnRForm.getPeriodEnd(),is(periodEnd));
+        testPeriodBeginEnd(periodBegin, periodEnd);
     }
 
     @Test
     public void shouldGetPeriodByPeriodBeginAtCriticalPoint() throws Exception {
-        RnRForm rnRForm=new RnRForm();
-        Date periodBegin = DateUtil.parseString("21/12/2015", DateUtil.SIMPLE_DATE_FORMAT);
-        rnRForm.setPeriodBegin(periodBegin);
-        RnRForm.setPeriodByPeriodBegin(rnRForm);
+        Date periodBegin = parseString("21/12/2015", SIMPLE_DATE_FORMAT);
+        Date periodEnd = parseString("20/01/2016", SIMPLE_DATE_FORMAT);
 
-        assertThat(rnRForm.getPeriodBegin(), is(periodBegin));
-        Date periodEnd = DateUtil.parseString("20/01/2016", DateUtil.SIMPLE_DATE_FORMAT);
-        assertThat(rnRForm.getPeriodEnd(),is(periodEnd));
+        testPeriodBeginEnd(periodBegin, periodEnd);
     }
 
+    private void testPeriodBeginEnd(Date givenBegin, Date expectedEnd) {
+        RnRForm rnRForm = new RnRForm();
+        rnRForm.setPeriodBegin(givenBegin);
+        RnRForm.setPeriodByPeriodBegin(rnRForm);
+
+        assertThat(rnRForm.getPeriodBegin(), is(givenBegin));
+        assertThat(rnRForm.getPeriodEnd(), is(expectedEnd));
+    }
 
 }
