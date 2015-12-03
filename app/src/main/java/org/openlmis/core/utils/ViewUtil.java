f0730406c5654cd308ac8e90b95045e73575c2ae
sherlockHK <576717731@qq.com>
2015-12-03 17:44:00 +0800

    ZS/HK -#425 -fix checkstyle

--- app/src/main/java/org/openlmis/core/utils/ViewUtil.java
+++ app/src/main/java/org/openlmis/core/utils/ViewUtil.java
@@ -4,7 +4,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 
-public class ViewUtil {
+public final class ViewUtil {
     private ViewUtil() {
     }
 
