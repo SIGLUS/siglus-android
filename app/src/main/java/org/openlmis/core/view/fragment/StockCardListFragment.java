6ff7b6ddd4bbe0c38ae85e1dda7e82026d67e091
gongming <gongmingqm10@foxmail.com>
2015-12-15 17:34:27 +0800

    MG - #000 - Fix layout overdraw issue

--- app/src/main/java/org/openlmis/core/view/fragment/StockCardListFragment.java
+++ app/src/main/java/org/openlmis/core/view/fragment/StockCardListFragment.java
@@ -101,7 +101,7 @@ public class StockCardListFragment extends BaseFragment implements StockCardPres
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_stockcard_list, container, false);
+        return inflater.inflate(R.layout.fragment_stockcard_list, container, true);
     }
 
     @Override
