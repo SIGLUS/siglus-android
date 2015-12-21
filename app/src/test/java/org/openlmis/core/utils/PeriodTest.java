904ed6842e67f7ddc8ef4fc6434319c8934f820c
cuipengfei <cuipengfei2008@gmail.com>
2015-12-21 12:34:34 +0800

    CPF - #000 - more cases to test period

--- app/src/test/java/org/openlmis/core/utils/PeriodTest.java
+++ app/src/test/java/org/openlmis/core/utils/PeriodTest.java
@@ -9,14 +9,21 @@ import static org.junit.Assert.assertThat;
 public class PeriodTest {
     @Test
     public void shouldDetermineItsOwnBeginAndEnd() throws Exception {
+        testPeriodBeginEnd("2015-02-03", "2015-01-21", "2015-02-20");//normal date
+        testPeriodBeginEnd("2016-08-20", "2016-07-21", "2016-08-20");//on end
+        testPeriodBeginEnd("2014-06-21", "2014-06-21", "2014-07-20");//on begin
+        testPeriodBeginEnd("2014-12-25", "2014-12-21", "2015-01-20");//cross year
+    }
+
+    private void testPeriodBeginEnd(String anyDayInPeriod, String begin, String end) {
         //given
-        DateTime feb03 = DateTime.parse("2015-02-03");
+        DateTime anyDay = DateTime.parse(anyDayInPeriod);
 
         //when
-        Period period = new Period(feb03);
+        Period period = new Period(anyDay);
 
         //then
-        assertThat(period.getBegin(), is(DateTime.parse("2015-01-21")));
-        assertThat(period.getEnd(), is(DateTime.parse("2015-02-20")));
+        assertThat(period.getBegin(), is(DateTime.parse(begin)));
+        assertThat(period.getEnd(), is(DateTime.parse(end)));
     }
 }
