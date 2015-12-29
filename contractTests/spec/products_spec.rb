9dab5921e511eb7c800fbae3120433aed2a40f03
sherlockHK <576717731@qq.com>
2015-12-29 11:01:25 +0800

    DY&HK -515 -add contract test for latestProgramWithProducts API

--- contractTests/spec/products_spec.rb
+++ contractTests/spec/products_spec.rb
@@ -26,7 +26,7 @@ describe "Get programs with products information for a facility" do
     facility_id = JSON.parse(login_response.body)['userInformation']['facilityId']
 
     #if no parameter for afterUpdatedTime, server will return all the products for programs associated with that facility;
-    #if afterUpdatedTime is specificed, server will only return products updated after "afterUpdatedTime"
+    #if afterUpdatedTime is specified, server will only return products updated after "afterUpdatedTime"
     response = RestClient.get "http://#{WEB_DEV_URI}/rest-api/latest-programs-with-products?facilityId=#{facility_id}",
       :content_type => :json,
       :accept => :json,
@@ -35,11 +35,11 @@ describe "Get programs with products information for a facility" do
     body = JSON.parse(response.body)
     expect(response.code).to eq 200
 
-    expect(body['latestProgramsWithProducts'].length).to eq 2
-    expect(body['latestProgramsWithProducts'][0]['programCode']).not_to be_nil
-    expect(body['latestProgramsWithProducts'][0]['programName']).not_to be_nil
+    expect(body['latestProgramsWithProducts']['programWithProductsList'].length).to eq 2
+    expect(body['latestProgramsWithProducts']['latestUpdatedTime']).not_to be_nil
+    expect(body['latestProgramsWithProducts']['programWithProductsList'][0]['programName']).not_to be_nil
 
-    expect(body['latestProgramsWithProducts'][0]['products'][0]['code']).not_to be_nil
-    expect(body['latestProgramsWithProducts'][0]['products'][0]['primaryName']).not_to be_nil
+    expect(body['latestProgramsWithProducts']['programWithProductsList'][0]['products'][0]['code']).not_to be_nil
+    expect(body['latestProgramsWithProducts']['programWithProductsList'][0]['products'][0]['primaryName']).not_to be_nil
   end
 end
