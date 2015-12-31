bd5596ca4d1f4ca6ea0939b4240d573b41301ac4
Cedric Song <tcsong@thoughtworks.com>
2015-12-31 10:34:54 +0800

    DY&CS - #242 - deserialize "active" field from server products

--- app/src/main/java/org/openlmis/core/model/Product.java
+++ app/src/main/java/org/openlmis/core/model/Product.java
@@ -21,9 +21,9 @@ package org.openlmis.core.model;
 
 import android.support.annotation.NonNull;
 
+import com.google.gson.annotations.SerializedName;
 import com.j256.ormlite.field.DatabaseField;
 import com.j256.ormlite.table.DatabaseTable;
-
 
 import lombok.Getter;
 import lombok.Setter;
@@ -55,6 +55,7 @@ public class Product extends BaseModel implements Comparable<Product> {
     @DatabaseField
     boolean isArchived;
 
+    @SerializedName("active")
     @DatabaseField
     boolean isActive;
 
