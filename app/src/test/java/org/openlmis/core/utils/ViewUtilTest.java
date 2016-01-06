2e259d956fea295fa7ca501338326bd834b14d8f
sun zhuo <zsun@thoughtworks.com>
2016-01-06 16:53:28 +0800

    ZS&CS - #372 -  add test for check edit text empty

--- app/src/test/java/org/openlmis/core/utils/ViewUtilTest.java
+++ app/src/test/java/org/openlmis/core/utils/ViewUtilTest.java
@@ -2,6 +2,7 @@ package org.openlmis.core.utils;
 
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import junit.framework.TestCase;
 
@@ -9,6 +10,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openlmis.core.LMISApp;
 import org.openlmis.core.LMISTestRunner;
+import org.openlmis.core.R;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(LMISTestRunner.class)
 public class ViewUtilTest extends TestCase {
@@ -22,5 +26,13 @@ public class ViewUtilTest extends TestCase {
         ViewUtil.syncViewHeight(leftView, rightView);
 
         assertEquals(leftView.getHeight(),rightView.getHeight());
+    }
+
+    @Test
+    public void shouldSetErrorWhenEditTextIsEmpty() throws Exception {
+        EditText editText = new EditText(LMISApp.getContext());
+        boolean checkEditTextEmpty = ViewUtil.checkEditTextEmpty(editText);
+        assertFalse(checkEditTextEmpty);
+        assertThat(editText.getError().toString()).isEqualTo(LMISApp.getContext().getString(R.string.hint_error_input));
     }
 }
