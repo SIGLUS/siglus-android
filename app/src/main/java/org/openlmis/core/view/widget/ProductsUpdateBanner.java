2d9ba3a902a7ec0e19b63b0eb1e9a8adcc9b9a5f
sun zhuo <zsun@thoughtworks.com>
2016-01-08 15:21:49 +0800

    ZS&ZY - #529 - add click event

--- app/src/main/java/org/openlmis/core/view/widget/ProductsUpdateBanner.java
+++ app/src/main/java/org/openlmis/core/view/widget/ProductsUpdateBanner.java
@@ -3,14 +3,19 @@ package org.openlmis.core.view.widget;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.LinearLayout;
 
 import org.openlmis.core.LMISApp;
 import org.openlmis.core.R;
 
 import roboguice.RoboGuice;
+import roboguice.inject.InjectView;
 
-public class ProductsUpdateBanner extends LinearLayout {
+public class ProductsUpdateBanner extends LinearLayout implements View.OnClickListener{
+
+    @InjectView(R.id.iv_product_update_banner_clear)
+    View ivClear;
 
     public ProductsUpdateBanner(Context context) {
         super(context);
@@ -30,5 +35,16 @@ public class ProductsUpdateBanner extends LinearLayout {
         if (!LMISApp.getInstance().getFeatureToggleFor(R.bool.feature_show_products_update_banner_529)) {
             setVisibility(GONE);
         }
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        ivClear.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        setVisibility(GONE);
     }
 }
