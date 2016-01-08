337aa93e860a2bb5f2f6250d00981c61086b94ce
sun zhuo <zsun@thoughtworks.com>
2016-01-08 18:27:53 +0800

    ZS&ZY - #529 - set banner text

--- app/src/main/java/org/openlmis/core/view/widget/ProductsUpdateBanner.java
+++ app/src/main/java/org/openlmis/core/view/widget/ProductsUpdateBanner.java
@@ -5,10 +5,13 @@ import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import org.openlmis.core.LMISApp;
 import org.openlmis.core.R;
 import org.openlmis.core.manager.SharedPreferenceMgr;
+
+import java.util.Set;
 
 import roboguice.RoboGuice;
 import roboguice.inject.InjectView;
@@ -17,6 +20,9 @@ public class ProductsUpdateBanner extends LinearLayout implements View.OnClickLi
 
     @InjectView(R.id.iv_product_update_banner_clear)
     View ivClear;
+
+    @InjectView(R.id.tv_product_update)
+    TextView tvProductUpdate;
 
     public ProductsUpdateBanner(Context context) {
         super(context);
@@ -33,14 +39,24 @@ public class ProductsUpdateBanner extends LinearLayout implements View.OnClickLi
         RoboGuice.injectMembers(getContext(), this);
         RoboGuice.getInjector(getContext()).injectViewMembers(this);
 
-        if (SharedPreferenceMgr.getInstance().isNeedShowUpdateBanner()) {
+        if (SharedPreferenceMgr.getInstance().isNeedShowProductsUpdateBanner()) {
             setVisibility(VISIBLE);
+            setBannerText();
         } else {
             setVisibility(GONE);
         }
 
         if (!LMISApp.getInstance().getFeatureToggleFor(R.bool.feature_show_products_update_banner_529)) {
             setVisibility(GONE);
+        }
+    }
+
+    private void setBannerText() {
+        Set<String> showUpdateBannerText = SharedPreferenceMgr.getInstance().getShowUpdateBannerText();
+        if (showUpdateBannerText.size() == 1) {
+            tvProductUpdate.setText(getContext().getString(R.string.hint_update_banner_tips, showUpdateBannerText.toArray()[0]));
+        } else {
+            tvProductUpdate.setText(getContext().getString(R.string.hint_update_banner_tips, showUpdateBannerText.size() + " Products"));
         }
     }
 
@@ -53,6 +69,6 @@ public class ProductsUpdateBanner extends LinearLayout implements View.OnClickLi
     @Override
     public void onClick(View v) {
         setVisibility(GONE);
-        SharedPreferenceMgr.getInstance().setIsNeedShowUpdateBanner(false);
+        SharedPreferenceMgr.getInstance().setIsNeedShowProductsUpdateBanner(false, null);
     }
 }
