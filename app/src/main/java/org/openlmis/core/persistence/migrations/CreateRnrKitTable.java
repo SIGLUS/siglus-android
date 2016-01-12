a79ec67ab56761a1ce088424e819969630c5c57b
Yingli Yuan <ylyuan@thoughtworks.com>
2016-01-12 11:21:34 +0800

    YY/DY - #000 - fix checkstyle

--- app/src/main/java/org/openlmis/core/persistence/migrations/CreateRnrKitTable.java
+++ app/src/main/java/org/openlmis/core/persistence/migrations/CreateRnrKitTable.java
@@ -6,13 +6,13 @@ public class CreateRnrKitTable extends Migration{
 
     @Override
     public void up() {
-        execSQL("CREATE TABLE `rnr_kit_items` " +
-                "(`kitsReceived` BIGINT, " +
-                "`kitsOpened` BIGINT, " +
-                "`kitCode` VARCHAR NOT NULL REFERENCES products(code)," +
-                "`form_id` BIGINT NOT NULL, " +
-                "`createdAt` VARCHAR NOT NULL, " +
-                "`updatedAt` VARCHAR NOT NULL, " +
-                "`id` INTEGER PRIMARY KEY AUTOINCREMENT)");
+        execSQL("CREATE TABLE `rnr_kit_items` "
+                + "(`kitsReceived` BIGINT, "
+                + "`kitsOpened` BIGINT, "
+                + "`kitCode` VARCHAR NOT NULL REFERENCES products(code),"
+                + "`form_id` BIGINT NOT NULL, "
+                + "`createdAt` VARCHAR NOT NULL, "
+                + "`updatedAt` VARCHAR NOT NULL, "
+                + "`id` INTEGER PRIMARY KEY AUTOINCREMENT)");
     }
 }
