cb7b0ab7346db1f7031ad9547c481d2ab31fa0af
zhouyu11 <yuzhou@thoughtworks.com>
2016-01-13 17:07:54 +0800

    ZY&ZS -#529- should not show banner in kit page

--- app/src/main/java/org/openlmis/core/view/fragment/KitStockCardListFragment.java
+++ app/src/main/java/org/openlmis/core/view/fragment/KitStockCardListFragment.java
@@ -1,6 +1,13 @@
 package org.openlmis.core.view.fragment;
 
+import android.os.Bundle;
+import android.view.View;
+
+import org.openlmis.core.R;
 import org.openlmis.core.view.adapter.KitStockCardListAdapter;
+import org.openlmis.core.view.widget.ProductsUpdateBanner;
+
+import roboguice.inject.InjectView;
 
 public class KitStockCardListFragment extends StockCardListFragment {
     @Override
@@ -8,8 +15,18 @@ public class KitStockCardListFragment extends StockCardListFragment {
         presenter.loadKits();
     }
 
+
+    @InjectView(R.id.product_update_banner)
+    ProductsUpdateBanner productsUpdateBanner;
+
     @Override
     protected void createAdapter() {
         mAdapter = new KitStockCardListAdapter(stockCardViewModels, onItemViewClickListener);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        productsUpdateBanner.setVisibility(View.GONE);
     }
 }
