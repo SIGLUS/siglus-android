a318887a2d01b7b48779f8a16463ac2b0c659a37
zhouyu11 <yuzhou@thoughtworks.com>
2016-01-18 16:50:29 +0800

    CS -#000 - use bash exec script

--- functionalTests/features/step_definitions/server_data_steps.rb
+++ functionalTests/features/step_definitions/server_data_steps.rb
@@ -1,23 +1,23 @@
 Given(/^Server updates drug data/) do
-  system("cd #{LMIS_MOZ_DIR} && ./data/functional_tests/update_products.sh")
+  system("cd #{LMIS_MOZ_DIR} && bash ./data/functional_tests/update_products.sh")
 end
 
 Given(/^Server updates stock_movements data/) do
-  system("cd #{LMIS_MOZ_DIR} && ./data/functional_tests/update_stock_movements.sh")
+  system("cd #{LMIS_MOZ_DIR} && bash ./data/functional_tests/update_stock_movements.sh")
 end
 
 Then(/^I clean up server drug data which I updated/) do
-  system("cd #{LMIS_MOZ_DIR} && ./data/functional_tests/rollback_updated_products.sh")
+  system("cd #{LMIS_MOZ_DIR} && bash ./data/functional_tests/rollback_updated_products.sh")
 end
 
 Given(/^server deactivates products 12D03 and 07L01/) do
-  system("cd #{LMIS_MOZ_DIR} && ./data/functional_tests/deactivate_products.sh")
+  system("cd #{LMIS_MOZ_DIR} && bash ./data/functional_tests/deactivate_products.sh")
 end
 
 When(/^server reactive products/) do
-  system("cd #{LMIS_MOZ_DIR} && ./data/functional_tests/reactivate_products.sh")
+  system("cd #{LMIS_MOZ_DIR} && bash ./data/functional_tests/reactivate_products.sh")
 end
 
 Given(/^server deactivates products has stock movement/) do
-  system("cd #{LMIS_MOZ_DIR} && ./data/functional_tests/deactivate_products_have_stock_movement.sh")
+  system("cd #{LMIS_MOZ_DIR} && bash ./data/functional_tests/deactivate_products_have_stock_movement.sh")
 end
