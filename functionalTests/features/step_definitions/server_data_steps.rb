cfaaac5f03d0751f2b670ab12b15d3d1830aaaba
Yingli Yuan <ylyuan@thoughtworks.com>
2016-02-03 11:13:09 +0800

    YL- #000 - run low stock cmm as regression tests

--- functionalTests/features/step_definitions/server_data_steps.rb
+++ functionalTests/features/step_definitions/server_data_steps.rb
@@ -3,7 +3,13 @@ Given(/^Server updates drug data/) do
 end
 
 Given(/^Server updates stock_movements data/) do
-  system("cd #{LMIS_MOZ_DIR} && bash ./data/functional_tests/update_stock_movements.sh")
+  if !Dir.exists?(LMIS_MOZ_DIR)
+    system("git clone https://github.com/clintonhealthaccess/lmis-moz.git #{LMIS_MOZ_DIR}")
+  else
+    system("cd #{LMIS_MOZ_DIR} && git checkout . && git pull -f origin master")
+  end
+
+  system("cd #{LMIS_MOZ_DIR} && bash ./data/functional_tests/regression/update_stock_movements.sh")
 end
 
 Then(/^I clean up server drug data which I updated/) do
