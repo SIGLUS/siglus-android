404ebe85c0856154e003115b0d6460de7fe812fa
cuipengfei <cuipengfei2008@gmail.com>
2016-02-04 10:04:42 +0800

    CPF -000- remove debug info

--- functionalTests/features/step_definitions/kit_unpack_steps.rb
+++ functionalTests/features/step_definitions/kit_unpack_steps.rb
@@ -15,7 +15,6 @@ end
 
 Then(/^I click the last sync banner$/) do
     last_sync_banner=query("android.widget.TextView id:'tx_sync_time'")
-    binding.pry
     fail "can not find the last sync banner" if last_sync_banner.nil?
     touch(last_sync_banner)
 end
