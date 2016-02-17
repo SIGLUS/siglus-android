8d4a01365b9530e902951ba171f07ae204ee0e2c
Yingli Yuan <ylyuan@thoughtworks.com>
2016-02-17 17:25:30 +0800

    YL&ZS- #579 - add functional test for unpack multiple kit once

--- functionalTests/features/step_definitions/kit_unpack_steps.rb
+++ functionalTests/features/step_definitions/kit_unpack_steps.rb
@@ -13,6 +13,20 @@ And(/^I enter quantity for all products in kit$/) do
         tap_when_element_exists("* marked:'Complete'")
 end
 
+And(/^I enter quantity "(.*?)" for first product$/) do |quantity|
+    h = query("android.widget.EditText id:'tx_quantity'").first
+    clear_text_in(h)
+    touch(h)
+    keyboard_enter_text(quantity)
+    hide_soft_keyboard
+end
+
+And(/^I should see "(.*?)" in quantity and expected quantity$/) do |number|
+    unless (element_exists("android.widget.EditText id:'tx_quantity'' text:'#{number}'") && element_exists("android.widget.TextView id:'stock_on_hand_in_inventory'' text:'#{number}'"))
+        fail(msg="quantity invalid")
+    end
+end
+
 Then(/^I click the last sync banner$/) do
     last_sync_banner=query("android.widget.TextView id:'tx_sync_time'")
     fail "can not find the last sync banner" if last_sync_banner.nil?
