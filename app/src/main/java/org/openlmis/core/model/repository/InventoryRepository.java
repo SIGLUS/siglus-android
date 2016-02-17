70b98c4d1f04ec045164c7701aa368fb3f43fccf
gongming <gongmingqm10@foxmail.com>
2016-02-17 15:21:58 +0800

    MG/DY - #456 - query initial inventory date

--- app/src/main/java/org/openlmis/core/model/repository/InventoryRepository.java
+++ app/src/main/java/org/openlmis/core/model/repository/InventoryRepository.java
@@ -44,7 +44,12 @@ public class InventoryRepository {
         });
     }
 
-    public Inventory queryInitialInventory() {
-        return null;
+    public Inventory queryInitialInventory() throws LMISException {
+        return dbUtil.withDao(Inventory.class, new DbUtil.Operation<Inventory, Inventory>() {
+            @Override
+            public Inventory operate(Dao<Inventory, String> dao) throws SQLException {
+                return dao.queryBuilder().orderBy("updatedAt", false).queryForFirst();
+            }
+        });
     }
 }
