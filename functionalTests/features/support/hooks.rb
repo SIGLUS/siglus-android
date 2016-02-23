5a09fb9553931fa934cf5f9b7eb13cffb74a8533
zhouyu11 <yuzhou@thoughtworks.com>
2016-02-23 16:26:42 +0800

    ZY&HK -#000 - set time for default device

--- functionalTests/features/support/hooks.rb
+++ functionalTests/features/support/hooks.rb
@@ -15,12 +15,22 @@ Before('@reinstall_app') do
 end
 
 Before('@change_date') do
-  system('adb -s $ADB_DEVICE_ARG shell "su 0 date -s 20160121.130000"')
+  if ENV["ADB_DEVICE_ARG"].nil?
+    system('adb shell "su 0 date -s 20160121.130000"')
+  else
+    system('adb -s $ADB_DEVICE_ARG shell "su 0 date -s 20160121.130000"')
+  end
 end
 
 After('@change_date') do
   current_time = Time.now.strftime("%Y%m%d.%H%M%S")
   system("adb -s $ADB_DEVICE_ARG shell 'su 0 date -s #{current_time}'")
+
+  if ENV["ADB_DEVICE_ARG"].nil?
+    system("adb shell 'su 0 date -s #{current_time}'")
+  else
+    system("adb -s $ADB_DEVICE_ARG shell 'su 0 date -s #{current_time}'")
+  end
 end
 
 def reset_local_server_data
