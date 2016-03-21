e9d847bc5739c8dd681e95412e59f32e2de959bc
cuipengfei <cuipengfei2008@gmail.com>
2016-03-21 17:18:00 +0800

    cpf -000- skip self auto upgrade if it's within 18th-25th of a month

--- app/src/main/java/org/openlmis/core/service/UpgradeManager.java
+++ app/src/main/java/org/openlmis/core/service/UpgradeManager.java
@@ -3,8 +3,10 @@ package org.openlmis.core.service;
 import com.autoupdateapk.AutoUpdateApk;
 import com.google.inject.Singleton;
 
+import org.joda.time.DateTime;
 import org.openlmis.core.LMISApp;
 import org.openlmis.core.R;
+import org.openlmis.core.model.Period;
 
 @Singleton
 public class UpgradeManager {
@@ -12,6 +14,10 @@ public class UpgradeManager {
     private AutoUpdateApk autoUpdateApk = new AutoUpdateApk(LMISApp.getContext(), "", upgradeServerUrl);
 
     public void triggerUpgrade() {
+        if (Period.isWithinSubmissionWindow(DateTime.now())) {
+            return; //skip self auto upgrade if it's within 18th-25th of a month
+        }
+
         autoUpdateApk.checkUpdatesManually();
     }
 }
