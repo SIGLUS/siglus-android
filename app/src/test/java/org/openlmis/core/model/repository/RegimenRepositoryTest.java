fa2771e31b0532de97ca02bd0aebee22bc90c7ec
sun zhuo <zsun@thoughtworks.com>
2016-03-23 14:31:52 +0800

    ZS&YL- #219- add test for query by name

---
+++ app/src/test/java/org/openlmis/core/model/repository/RegimenRepositoryTest.java
@@ -0,0 +1,35 @@
+package org.openlmis.core.model.repository;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openlmis.core.LMISTestRunner;
+import org.openlmis.core.model.Regimen;
+import org.robolectric.RuntimeEnvironment;
+
+import roboguice.RoboGuice;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+@RunWith(LMISTestRunner.class)
+public class RegimenRepositoryTest {
+
+    private RegimenRepository repository;
+
+    @Before
+    public void setUp() throws Exception {
+        repository = RoboGuice.getInjector(RuntimeEnvironment.application).getInstance(RegimenRepository.class);
+    }
+
+    @Test
+    public void shouldQueryByNameSuccessful() throws Exception {
+        Regimen regimen = new Regimen();
+        String regimeName = "regimeName";
+        regimen.setName(regimeName);
+        repository.create(regimen);
+
+        Regimen actualRegime = repository.getByName(regimeName);
+        assertThat(actualRegime.getName(), is(regimeName));
+    }
+}
