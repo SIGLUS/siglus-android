3c6e0c73484e6ba10fd38bd19daaaf71db1c1450
sun zhuo <zsun@thoughtworks.com>
2016-03-28 14:37:42 +0800

    ZS- #219- move config to constants

--- app/src/main/java/org/openlmis/core/presenter/ProductPresenter.java
+++ app/src/main/java/org/openlmis/core/presenter/ProductPresenter.java
@@ -9,6 +9,7 @@ import org.openlmis.core.model.Regimen;
 import org.openlmis.core.model.repository.ProductRepository;
 import org.openlmis.core.model.repository.ProgramRepository;
 import org.openlmis.core.model.repository.RegimenRepository;
+import org.openlmis.core.utils.Constants;
 import org.openlmis.core.view.BaseView;
 import org.openlmis.core.view.viewmodel.RegimeProductViewModel;
 import org.roboguice.shaded.goole.common.base.Function;
@@ -23,7 +24,6 @@ import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
 
 import static org.roboguice.shaded.goole.common.collect.FluentIterable.from;
-import static org.roboguice.shaded.goole.common.collect.Lists.newArrayList;
 
 public class ProductPresenter extends Presenter {
 
@@ -45,7 +45,7 @@ public class ProductPresenter extends Presenter {
             @Override
             public void call(Subscriber<? super List<RegimeProductViewModel>> subscriber) {
                 try {
-                    List<RegimeProduct> products = getRegimeProducts();
+                    List<RegimeProduct> products = Constants.getRegimeProducts();
                     List<RegimeProductViewModel> regimeProductViewModels = from(products).transform(new Function<RegimeProduct, RegimeProductViewModel>() {
                         @Override
                         public RegimeProductViewModel apply(RegimeProduct product) {
@@ -60,30 +60,6 @@ public class ProductPresenter extends Presenter {
                 }
             }
         }).observeOn(AndroidSchedulers.mainThread()).subscribeOn(Schedulers.io());
-    }
-
-    private ArrayList<RegimeProduct> getRegimeProducts() throws LMISException {
-        return newArrayList(
-                new RegimeProduct("3TC 150mg", "Lamivudina 150mg"),
-                new RegimeProduct("3TC 300 mg", "Lamivudina 300mg"),
-                new RegimeProduct("AZT 300 mg", "zidovudina 300mg"),
-                new RegimeProduct("NVP 200 mg", "Nevirapina 200mg"),
-                new RegimeProduct("TDF 300 mg", "Tenofovir 300mg"),
-                new RegimeProduct("EFV 600 mg", "Efavirenze 600mg"),
-                new RegimeProduct("Lpv/r 200/50mg", "Lopinavir/Ritonavir 200/50 mg"),
-                new RegimeProduct("ABC 300mg", "Abacavir 300mg"),
-                new RegimeProduct("D4T 30mg", "Stavudina 30mg"),
-                new RegimeProduct("3TC30mg", "Lamivudina 30mg"),
-                new RegimeProduct("D4T 6mg", "Stavudina 6mg"),
-                new RegimeProduct("AZT60mg", "Zidovudina60mg"),
-                new RegimeProduct("NVP 50mg", "Nevirapina 50mg"),
-                new RegimeProduct("Lpv/r 100/25mg", "Lopinavir/Ritonavir 100/25mg"),
-                new RegimeProduct("Lpv/r 80/20mL Solucao oral", "Lopinavir/Ritonavir 80/20mL Solucao oral"),
-                new RegimeProduct("EFV 200mg", "Efavirenze 200mg"),
-                new RegimeProduct("EFV 50mg", "Efavirenze 50mg"),
-                new RegimeProduct("ABC60mg", "Abacavir 60mg"),
-                new RegimeProduct("NVP 50mg/5ml sol oral", "Nevirapina 50mg/5ml sol oral"),
-                new RegimeProduct("AZT 50mg/5ml sol oral", "zidovudina 50mg/5ml sol oral"));
     }
 
     public Observable<Regimen> saveRegimes(List<RegimeProductViewModel> viewModels, final Regimen.RegimeType regimeType) {
