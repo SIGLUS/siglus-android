0f8cc0179e4c9b86dc4b8ccf5d5787883736f73a
sun zhuo <zsun@thoughtworks.com>
2016-03-31 15:58:23 +0800

    ZS - #000- add test for via repository

--- app/src/test/java/org/openlmis/core/model/repository/VIARepositoryTest.java
+++ app/src/test/java/org/openlmis/core/model/repository/VIARepositoryTest.java
@@ -1,44 +1,47 @@
 package org.openlmis.core.model.repository;
 
-import com.google.inject.AbstractModule;
-
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openlmis.core.LMISTestRunner;
 import org.openlmis.core.exceptions.LMISException;
+import org.openlmis.core.model.BaseInfoItem;
 import org.openlmis.core.model.Program;
+import org.openlmis.core.model.RnRForm;
 import org.robolectric.RuntimeEnvironment;
+
+import java.util.List;
 
 import roboguice.RoboGuice;
 
-import static org.mockito.Mockito.mock;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 
 @RunWith(LMISTestRunner.class)
 public class VIARepositoryTest {
 
-    private StockRepository mockStockRepository;
-    private ProductRepository productRepository;
     private VIARepository viaRepository;
     private Program viaProgram;
 
     @Before
     public void setup() throws LMISException {
-
-        mockStockRepository = mock(StockRepository.class);
-        productRepository = mock(ProductRepository.class);
-        RoboGuice.overrideApplicationInjector(RuntimeEnvironment.application, new MyTestModule());
-
         viaRepository = RoboGuice.getInjector(RuntimeEnvironment.application).getInstance(VIARepository.class);
 
         viaProgram = new Program("VIA", "VIA", null, null);
         viaProgram.setId(1l);
     }
 
-    public class MyTestModule extends AbstractModule {
-        @Override
-        protected void configure() {
-            bind(StockRepository.class).toInstance(mockStockRepository);
-            bind(ProductRepository.class).toInstance(productRepository);
-        }
+    @Test
+    public void shouldGenerateBaseInfoItems() throws Exception {
+        RnRForm form = new RnRForm();
+        form.setProgram(viaProgram);
+        form.setComments("Submitted Form");
+        form.setStatus(RnRForm.STATUS.AUTHORIZED);
+        form.setSynced(false);
+
+        List<BaseInfoItem> baseInfoItems = viaRepository.generateBaseInfoItems(form);
+
+        assertThat(baseInfoItems.size(), is(1));
+        assertThat(baseInfoItems.get(0).getName(), is(VIARepository.ATTR_CONSULTATION));
     }
 }
