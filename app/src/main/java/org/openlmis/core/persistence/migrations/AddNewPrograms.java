071f54a5e1b6b4eb7198a4e12285037f0cfa113e
Danni Yu <dyu@thoughtworks.com>
2016-04-05 17:04:48 +0800

    DY - #000 - fix checkstyle

--- app/src/main/java/org/openlmis/core/persistence/migrations/AddNewPrograms.java
+++ app/src/main/java/org/openlmis/core/persistence/migrations/AddNewPrograms.java
@@ -12,36 +12,36 @@ public class AddNewPrograms extends Migration {
     public void up() {
         String formatDate = DateUtil.formatDate(new Date(LMISApp.getInstance().getCurrentTimeMillis()), DateUtil.DATE_TIME_FORMAT);
 
-        execSQL("INSERT INTO programs (programCode, programName, createdAt, updatedAt) " +
-                "SELECT 'VIA', 'VIA', '" + formatDate + "' , '"  + formatDate + "' " +
-                "WHERE NOT EXISTS (SELECT * FROM programs WHERE programCode = 'VIA')");
+        execSQL("INSERT INTO programs (programCode, programName, createdAt, updatedAt) "
+                + "SELECT 'VIA', 'VIA', '" + formatDate + "' , '"  + formatDate + "' "
+                + "WHERE NOT EXISTS (SELECT * FROM programs WHERE programCode = 'VIA')");
 
-        execSQL("INSERT INTO programs (programCode, programName, createdAt, updatedAt, parentCode) " +
-                "SELECT 'TEST_KIT', 'Testes Rápidos Diag', '"  + formatDate + "' , '" + formatDate +   "', 'VIA' " +
-                "WHERE NOT EXISTS (SELECT * FROM programs WHERE programCode = 'TEST_KIT')");
+        execSQL("INSERT INTO programs (programCode, programName, createdAt, updatedAt, parentCode) "
+                + "SELECT 'TEST_KIT', 'Testes Rápidos Diag', '"  + formatDate + "' , '" + formatDate +   "', 'VIA' "
+                + "WHERE NOT EXISTS (SELECT * FROM programs WHERE programCode = 'TEST_KIT')");
 
-        execSQL("INSERT INTO programs (programCode, programName, createdAt, updatedAt, parentCode) " +
-                "SELECT 'TB', 'Tuberculose', '" + formatDate + "' , '" + formatDate +  "', 'VIA' " +
-                "WHERE NOT EXISTS (SELECT * FROM programs WHERE programCode = 'TB')");
+        execSQL("INSERT INTO programs (programCode, programName, createdAt, updatedAt, parentCode) "
+                + "SELECT 'TB', 'Tuberculose', '" + formatDate + "' , '" + formatDate +  "', 'VIA' "
+                + "WHERE NOT EXISTS (SELECT * FROM programs WHERE programCode = 'TB')");
 
-        execSQL("INSERT INTO programs (programCode, programName, createdAt, updatedAt, parentCode) " +
-                "SELECT 'MALARIA', 'Malaria', '" + formatDate + "' , '" + formatDate +  "', 'VIA' " +
-                "WHERE NOT EXISTS (SELECT * FROM programs WHERE programCode = 'MALARIA')");
+        execSQL("INSERT INTO programs (programCode, programName, createdAt, updatedAt, parentCode) "
+                + "SELECT 'MALARIA', 'Malaria', '" + formatDate + "' , '" + formatDate +  "', 'VIA' "
+                + "WHERE NOT EXISTS (SELECT * FROM programs WHERE programCode = 'MALARIA')");
 
-        execSQL("INSERT INTO programs (programCode, programName, createdAt, updatedAt, parentCode) " +
-                "SELECT 'PTV', 'PTV', '" + formatDate + "' , '" + formatDate + "', 'MMIA' " +
-                "WHERE NOT EXISTS (SELECT * FROM programs WHERE programCode = 'PTV')");
+        execSQL("INSERT INTO programs (programCode, programName, createdAt, updatedAt, parentCode) "
+                + "SELECT 'PTV', 'PTV', '" + formatDate + "' , '" + formatDate + "', 'MMIA' "
+                + "WHERE NOT EXISTS (SELECT * FROM programs WHERE programCode = 'PTV')");
 
-        execSQL("INSERT INTO programs (programCode, programName, createdAt, updatedAt, parentCode) " +
-                "SELECT 'TARV', 'TARV', '" + formatDate + "' , '" + formatDate + "', 'MMIA' " +
-                "WHERE NOT EXISTS (SELECT * FROM programs WHERE programCode = 'TARV')");
+        execSQL("INSERT INTO programs (programCode, programName, createdAt, updatedAt, parentCode) "
+                + "SELECT 'TARV', 'TARV', '" + formatDate + "' , '" + formatDate + "', 'MMIA' "
+                + "WHERE NOT EXISTS (SELECT * FROM programs WHERE programCode = 'TARV')");
 
-        execSQL("INSERT INTO programs (programCode, programName, createdAt, updatedAt, parentCode) " +
-                "SELECT 'PME', 'PME', '" + formatDate + "' , '" + formatDate + "', 'VIA' " +
-                "WHERE NOT EXISTS (SELECT * FROM programs WHERE programCode = 'PME')");
+        execSQL("INSERT INTO programs (programCode, programName, createdAt, updatedAt, parentCode) "
+                + "SELECT 'PME', 'PME', '" + formatDate + "' , '" + formatDate + "', 'VIA' "
+                + "WHERE NOT EXISTS (SELECT * FROM programs WHERE programCode = 'PME')");
 
-        execSQL("INSERT INTO programs (programCode, programName, createdAt, updatedAt, parentCode) " +
-                "SELECT 'NUTRITION', 'NUTRITION', '" + formatDate + "' , '" + formatDate + "', 'VIA' " +
-                "WHERE NOT EXISTS (SELECT * FROM programs WHERE programCode = 'NUTRITION')");
+        execSQL("INSERT INTO programs (programCode, programName, createdAt, updatedAt, parentCode) "
+                + "SELECT 'NUTRITION', 'NUTRITION', '" + formatDate + "' , '" + formatDate + "', 'VIA' "
+                + "WHERE NOT EXISTS (SELECT * FROM programs WHERE programCode = 'NUTRITION')");
     }
 }
--- app/src/main/java/org/openlmis/core/persistence/migrations/ConvertEssMedsToVIAProgram.java
+++ app/src/main/java/org/openlmis/core/persistence/migrations/ConvertEssMedsToVIAProgram.java
@@ -5,7 +5,7 @@ import org.openlmis.core.persistence.Migration;
 public class ConvertEssMedsToVIAProgram extends Migration {
     @Override
     public void up() {
-        execSQL("UPDATE rnr_forms SET program_id = (SELECT id FROM programs WHERE programCode = 'VIA') " +
-                "WHERE program_id = (SELECT id FROM programs WHERE programCode = 'ESS_MEDS')");
+        execSQL("UPDATE rnr_forms SET program_id = (SELECT id FROM programs WHERE programCode = 'VIA') "
+                + "WHERE program_id = (SELECT id FROM programs WHERE programCode = 'ESS_MEDS')");
     }
 }
