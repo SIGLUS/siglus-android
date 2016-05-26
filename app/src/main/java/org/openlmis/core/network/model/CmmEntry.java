90e8077b51189610e0071343c15dbb72359eba57
cuipengfei <cuipengfei2008@gmail.com>
2016-05-26 15:25:48 +0800

    cpf -752- cmm entry model for http traffic

---
+++ app/src/main/java/org/openlmis/core/network/model/CmmEntry.java
@@ -0,0 +1,30 @@
+package org.openlmis.core.network.model;
+
+import org.openlmis.core.model.Cmm;
+
+import static org.openlmis.core.utils.DateUtil.DB_DATE_FORMAT;
+import static org.openlmis.core.utils.DateUtil.formatDate;
+
+public class CmmEntry {
+    String productCode;
+
+    float cmmValue;
+
+    String periodBegin;
+
+    String periodEnd;
+
+    public CmmEntry(String productCode, float cmmValue, String periodBegin, String periodEnd) {
+        this.productCode = productCode;
+        this.cmmValue = cmmValue;
+        this.periodBegin = periodBegin;
+        this.periodEnd = periodEnd;
+    }
+
+    public static CmmEntry createFrom(Cmm cmm) {
+        return new CmmEntry(cmm.getStockCard().getProduct().getCode(),
+                cmm.getCmmValue(),
+                formatDate(cmm.getPeriodBegin(), DB_DATE_FORMAT),
+                formatDate(cmm.getPeriodBegin(), DB_DATE_FORMAT));
+    }
+}
