d680eca62362336dbd4a7722e32e138981cb1639
Shengzhi Wang <594321736@qq.com>
2016-07-26 18:41:44 +0800

    WSZ  - #815 - fix unit test when set delete icon for added product in via form

--- app/src/test/java/org/openlmis/core/view/holder/RequisitionProductViewHolderTest.java
+++ app/src/test/java/org/openlmis/core/view/holder/RequisitionProductViewHolderTest.java
@@ -3,32 +3,60 @@ package org.openlmis.core.view.holder;
 import android.view.LayoutInflater;
 import android.view.View;
 
+import com.google.inject.Binder;
+import com.google.inject.Module;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openlmis.core.LMISTestApp;
 import org.openlmis.core.LMISTestRunner;
 import org.openlmis.core.R;
+import org.openlmis.core.model.Program;
 import org.openlmis.core.model.RnRForm;
 import org.openlmis.core.model.builder.ProductBuilder;
 import org.openlmis.core.model.RnrFormItem;
 import org.openlmis.core.model.builder.RnrFormItemBuilder;
 import org.openlmis.core.presenter.VIARequisitionPresenter;
+import org.openlmis.core.utils.DateUtil;
 import org.openlmis.core.view.viewmodel.RequisitionFormItemViewModel;
 import org.robolectric.RuntimeEnvironment;
 
+import java.sql.Date;
+
+import roboguice.RoboGuice;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @RunWith(LMISTestRunner.class)
 public class RequisitionProductViewHolderTest {
 
     private RequisitionProductViewHolder viewHolder;
     private VIARequisitionPresenter presenter;
-
+    private Program program;
+    private RnRForm form;
     @Before
     public void setUp() {
         View itemView = LayoutInflater.from(RuntimeEnvironment.application).inflate(R.layout.item_requisition_body_left, null, false);
         viewHolder = new RequisitionProductViewHolder(itemView);
+        program = new Program();
+        program.setProgramCode("ESS_MEDS");
+        program.setProgramName("ESS_MEDS");
+        form = RnRForm.init(program, DateUtil.today());
+        form.setPeriodBegin(Date.valueOf("2015-04-21"));
+        form.setPeriodEnd(Date.valueOf("2015-05-20"));
+
+        presenter = mock(VIARequisitionPresenter.class);
+        RoboGuice.overrideApplicationInjector(RuntimeEnvironment.application, new Module() {
+            @Override
+            public void configure(Binder binder) {
+                binder.bind(VIARequisitionPresenter.class).toInstance(presenter);
+            }
+        });
+        when(presenter.getRnRForm()).thenReturn(form);
+        when(presenter.getRnrFormStatus()).thenReturn(RnRForm.STATUS.DRAFT);
     }
 
     @Test
@@ -46,6 +74,7 @@ public class RequisitionProductViewHolderTest {
     @Test
     public void shouldSetDelIconForNewAddedProduct() throws Exception {
         LMISTestApp.getInstance().setFeatureToggle(R.bool.feature_add_drugs_to_via_form,true);
+
         RnrFormItem formItem = new RnrFormItemBuilder().setProduct(
                 new ProductBuilder().setPrimaryName("productName").setCode("08S42").build()).setRnrForm(null)
                 .build();
