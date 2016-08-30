c8088123e4bfb120d96383da576e54ce276805e3
Tianxi Ku <kutianxi@outlook.com>
2016-08-30 14:13:16 +0800

    TK/DY - #825 - add test for lot adapter

---
+++ app/src/test/java/org/openlmis/core/network/adapter/LotAdapterTest.java
@@ -0,0 +1,24 @@
+package org.openlmis.core.network.adapter;
+
+import com.google.gson.JsonParser;
+
+import org.junit.Test;
+import org.openlmis.core.model.Lot;
+import org.openlmis.core.utils.DateUtil;
+import org.openlmis.core.utils.JsonFileReader;
+
+import static org.junit.Assert.assertEquals;
+
+public class LotAdapterTest {
+    @Test
+    public void shouldCreateLotFromJson() throws Exception {
+        LotAdapter lotAdapter = new LotAdapter();
+
+        String json = JsonFileReader.readJson(getClass(), "Lot.json");
+
+        Lot lot = lotAdapter.deserialize(new JsonParser().parse(json), null, null);
+
+        assertEquals("TEST1", lot.getLotNumber());
+        assertEquals(DateUtil.parseString("2016-10-30", DateUtil.DB_DATE_FORMAT), lot.getExpirationDate());
+    }
+}
