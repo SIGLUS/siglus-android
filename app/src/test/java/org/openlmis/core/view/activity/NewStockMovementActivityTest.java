86042938c227300fa156bbe64edf052801161034
Tianxi Ku <kutianxi@outlook.com>
2016-11-11 18:16:24 +0800

    TK - #985 - fix unit test

--- app/src/test/java/org/openlmis/core/view/activity/NewStockMovementActivityTest.java
+++ app/src/test/java/org/openlmis/core/view/activity/NewStockMovementActivityTest.java
@@ -55,6 +55,7 @@ public class NewStockMovementActivityTest {
         LotMovementViewModel lot2 = new LotMovementViewModelBuilder().setLotNumber("test2").setLotSOH("100").setQuantity("50").build();
         stockMovementViewModel.getExistingLotMovementViewModelList().addAll(Arrays.asList(lot1,lot2));
 
+        stockMovementViewModel.setStockCard(new StockCardBuilder().setProduct(new ProductBuilder().build()).build());
         StockCard stockcard = new StockCardBuilder().setProduct(ProductBuilder.buildAdultProduct()).build();
 
         when(mockedPresenter.getStockMovementViewModel()).thenReturn(stockMovementViewModel);
