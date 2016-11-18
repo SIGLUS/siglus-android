6bbb2a5975361ee0108dec2d01ce10777448baa5
Tianxi Ku <kutianxi@outlook.com>
2016-11-18 12:19:52 +0800

    TK & WZ - #997 - add monkey test

---
+++ functionalTests/features/step_definitions/monkey_test_steps.rb
@@ -0,0 +1,18 @@
+require 'calabash-android/calabash_steps'
+require 'pry'
+
+Then(/^I do monkey test for "(\d+)" times/) do |times|
+
+  if ENV["ADB_DEVICE_ARG"].nil?
+    system("adb  shell 'cd data/data && monkey -p org.openlmis.core.prodsim -v #{times} > /sdcard/monkeytest.log'")
+  else
+    system("adb -s $ADB_DEVICE_ARG shell 'cd data/data && monkey -p org.openlmis.core.prodsim -v #{times} > /sdcard/monkeytest.log'")
+  end
+
+  system("adb pull /sdcard/monkeytest.log #{Dir.pwd}/monkeytest.log")
+
+  text = File.read("#{Dir.pwd}/monkeytest.log")
+  if text.include? "System appears to have crashed"
+    fail(msg="monkey test crashed")
+  end
+end
---
+++ functionalTests/features/stress_test/monkey_test.feature
@@ -0,0 +1,8 @@
+@monkey
+Feature: monkey test
+
+  Scenario: monkey test
+    Given I try to log in with "mali" "password1"
+    And I wait up to 120 seconds for "STOCK CARD OVERVIEW" to appear
+
+    Then I do monkey test for "100000" times
