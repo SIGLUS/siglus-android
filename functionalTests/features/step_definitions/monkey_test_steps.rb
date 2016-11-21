b46f15150b0f20f3646416f537a7f483b87a88de
Tianxi Ku <kutianxi@outlook.com>
2016-11-21 15:15:01 +0800

    TK - #000 - specify device in adb command

--- functionalTests/features/step_definitions/monkey_test_steps.rb
+++ functionalTests/features/step_definitions/monkey_test_steps.rb
@@ -5,11 +5,11 @@ Then(/^I do monkey test for "(\d+)" times/) do |times|
 
   if ENV["ADB_DEVICE_ARG"].nil?
     system("adb  shell 'cd data/data && monkey -p org.openlmis.core.prodsim -v #{times} > /sdcard/monkeytest.log'")
+    system("adb pull /sdcard/monkeytest.log ../../monkeytest.log")
   else
     system("adb -s $ADB_DEVICE_ARG shell 'cd data/data && monkey -p org.openlmis.core.prodsim -v #{times} > /sdcard/monkeytest.log'")
+    system("adb -s $ADB_DEVICE_ARG pull /sdcard/monkeytest.log ../../monkeytest.log")
   end
-
-  system("adb pull /sdcard/monkeytest.log ../../monkeytest.log")
 
   text = File.read("../../monkeytest.log")
   if text.include? "System appears to have crashed"
