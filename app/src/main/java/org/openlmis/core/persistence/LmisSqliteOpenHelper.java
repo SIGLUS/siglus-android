728e26eee0446cca0bc6d45208d5b3308f2a14d8
Danni Yu <dyu@thoughtworks.com>
2016-11-29 10:58:04 +0800

    TK/DY - #1005 - create new db table for program data

--- app/src/main/java/org/openlmis/core/persistence/LmisSqliteOpenHelper.java
+++ app/src/main/java/org/openlmis/core/persistence/LmisSqliteOpenHelper.java
@@ -57,6 +57,7 @@ import org.openlmis.core.persistence.migrations.CreateDummyRegimes;
 import org.openlmis.core.persistence.migrations.CreateInitTables;
 import org.openlmis.core.persistence.migrations.CreateKitProductsTable;
 import org.openlmis.core.persistence.migrations.CreateProductProgramsTable;
+import org.openlmis.core.persistence.migrations.CreateProgramDataFormTable;
 import org.openlmis.core.persistence.migrations.CreateRegimeShortCodeTable;
 import org.openlmis.core.persistence.migrations.CreateRnRFormSignature;
 import org.openlmis.core.persistence.migrations.SetQuantityOfStockMovementForInitialInventory;
@@ -113,6 +114,7 @@ public final class LmisSqliteOpenHelper extends OrmLiteSqliteOpenHelper {
             add(new AddLotMovementItemsTable());
             add(new AddLotOnHandTable());
             add(new CreateDraftLotMovementTable());
+            add(new CreateProgramDataFormTable());
         }
     };
     private static int instanceCount = 0;
