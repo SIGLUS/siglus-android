80adf7d90691575506187634acd93b946dd85192
Danni Yu <dyu@thoughtworks.com>
2016-12-06 15:56:36 +0800

    YD/DY - #1007 - add contract test for sync down

--- contractTests/spec/rapid_test_spec.rb
+++ contractTests/spec/rapid_test_spec.rb
@@ -36,7 +36,6 @@ describe "Sync stock card data" do
             value: 10
         }
         ]
-
     }
 
     response = RestClient.post "http://#{WEB_DEV_URI}/rest-api/programData",
@@ -45,5 +44,21 @@ describe "Sync stock card data" do
 
     body = JSON.parse(response.body)
     expect(response.code).to eq 200
+
+    response = RestClient.get "http://#{WEB_DEV_URI}/rest-api/programData/facilities/#{facility_id}",
+      :content_type => :json, :authorization => http_basic_auth('mystique', 'password1')
+    response_body = JSON.parse(response.body)
+
+    expect(response_body['programData'].length).to be 1
+    program_form = response_body['programData'][0]
+    expect(program_form['programCode']).to eq 'RAPID_TEST'
+    expect(program_form['periodBegin']).to eq 1455984000000
+    expect(program_form['periodEnd']).to eq 1458403200000
+    expect(program_form['submittedTime']).to eq 1480003200000
+
+    item1 = program_form['programDataFormItems'].detect do |item|
+        item['columnCode']=="HIV-DETERMINE-CONSUME" && item['name']=="PUB_PHARMACY"
+    end
+    expect(item1['value']).to eq 10
   end
 end
