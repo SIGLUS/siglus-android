d98e414133db48253ba873ce36606fe3344f09e2
Tianxi Ku <kutianxi@outlook.com>
2017-01-18 09:40:50 +0800

    TK - #1029 - fix bug

---
+++ app/src/main/java/org/openlmis/core/view/holder/StockMovementHistoryViewHolder.java
@@ -0,0 +1,16 @@
+package org.openlmis.core.view.holder;
+
+import android.view.View;
+
+import org.openlmis.core.view.viewmodel.StockMovementHistoryViewModel;
+
+public class StockMovementHistoryViewHolder extends BaseViewHolder{
+    public StockMovementHistoryViewHolder(View itemView) {
+        super(itemView);
+    }
+
+
+    public void populate(StockMovementHistoryViewModel viewModel) {
+
+    }
+}
