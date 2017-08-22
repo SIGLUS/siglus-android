7154d3ced7e6762522ab54c2f4c4871d524d4ea6
jrodri <jrodri@thoughtworks.com>
2017-08-22 16:32:44 -0500

    US52 DY/GR Add header to the recycleview

--- app/src/main/java/org/openlmis/core/view/adapter/BulkInitialInventoryAdapter.java
+++ app/src/main/java/org/openlmis/core/view/adapter/BulkInitialInventoryAdapter.java
@@ -4,26 +4,51 @@ import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
 import org.openlmis.core.R;
+import org.openlmis.core.view.holder.BaseViewHolder;
+import org.openlmis.core.view.holder.BulkInitialInventoryHeaderViewHolder;
 import org.openlmis.core.view.holder.BulkInitialInventoryViewHolder;
 import org.openlmis.core.view.viewmodel.InventoryViewModel;
 
 import java.util.List;
 
-public class BulkInitialInventoryAdapter extends InventoryListAdapter<BulkInitialInventoryViewHolder> {
+public class BulkInitialInventoryAdapter extends InventoryListAdapter<BaseViewHolder> {
+
+    private static int HEADER_POSITION = 0;
+    private static int ITEM_HEADER = 0;
+    private static int ITEM_LIST = 1;
 
     public BulkInitialInventoryAdapter(List<InventoryViewModel> data) {
         super(data);
     }
 
     @Override
-    public void onBindViewHolder(final BulkInitialInventoryViewHolder holder, final int position) {
-        final InventoryViewModel viewModel = filteredList.get(position);
-        holder.populate(viewModel, queryKeyWord);
+    public int getItemViewType(int position) {
+        if (position == HEADER_POSITION) {
+            return ITEM_HEADER;
+        } else {
+            return ITEM_LIST;
+        }
     }
 
+    @Override
+    public BaseViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        if (viewType == ITEM_LIST) {
+            return new BulkInitialInventoryViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.item_bulk_initial_inventory, parent, false));
+        } else {
+            return new BulkInitialInventoryHeaderViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.item_bulk_initial_inventory_header, parent, false));
+        }
+    }
 
     @Override
-    public BulkInitialInventoryViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return new BulkInitialInventoryViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.item_bulk_initial_inventory, parent, false));
+    public void onBindViewHolder(BaseViewHolder holder, int position) {
+        if (holder instanceof BulkInitialInventoryViewHolder) {
+            final InventoryViewModel viewModel = filteredList.get(position - 1);
+            ((BulkInitialInventoryViewHolder) holder).populate(viewModel, queryKeyWord);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return super.getItemCount() + 1;
     }
 }
---
+++ app/src/main/java/org/openlmis/core/view/holder/BulkInitialInventoryHeaderViewHolder.java
@@ -0,0 +1,9 @@
+package org.openlmis.core.view.holder;
+
+import android.view.View;
+
+public class BulkInitialInventoryHeaderViewHolder extends BaseViewHolder {
+    public BulkInitialInventoryHeaderViewHolder(View itemView) {
+        super(itemView);
+    }
+}
