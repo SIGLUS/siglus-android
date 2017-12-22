457f7ba436299b0d313ecd8583f7d9ecf4a05b2f
Cristian Toaquiza <ctoaqui@thoughtworks.com>
2017-12-22 17:51:42 -0500

    TT71 - FC/CT - Adds data container in unit tests execution

--- java_unit_test.sh
+++ java_unit_test.sh
@@ -2,8 +2,8 @@
 taskrunner="${TASKRUNNER:-siglus/test-runner:1.0.0}"
 pipelinename="${PIPELINE_NAME:-mobile_build}"
 dockernet="${DOCKER_NET:-gocd_default}"
-gradleexec="./gradlew clean testLocalDebug"
-LOCAL_DOCKER_SCRIPT="docker run --rm -v $(pwd):/openlmis-mobile --name test_local_debug -e JAVA_TOOL_OPTIONS=\"-Xmx4096m -XX:MaxPermSize=512m -Xms128m\" -e JAVA_OPTS=\"-XX:MaxPermSize=512m\" --memory 4096m -w /openlmis-mobile $taskrunner $gradleexec"
+gradleexec="./gradlew clean testLocalDebug -Dtest.single=org.openlmis.core.service.PatientDataServiceTest"
+LOCAL_DOCKER_SCRIPT="docker run --rm -v $(pwd):/openlmis-mobile -v gradle_cache:/root/.gradle --name test_local_debug -e JAVA_TOOL_OPTIONS=\"-Xmx4096m -XX:MaxPermSize=512m -Xms128m\" -e JAVA_OPTS=\"-XX:MaxPermSize=512m\" --memory 4096m -w /openlmis-mobile $taskrunner $gradleexec"
 CI_DOCKER_SCRIPT="docker run --rm --name test_local_debug --net $dockernet --volumes-from $HOSTNAME -w /godata/pipelines/$pipelinename $taskrunner $gradleexec"
 LOCAL_SCRIPT="$gradleexec"
 
