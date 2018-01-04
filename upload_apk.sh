addf6fde56601953325de6fa405068a6575ca596
faustocv <fcastane@thoughtworks.com>
2018-01-04 11:18:08 -0500

    TT74 - CT/FC - Refactors upload_apk script

--- upload_apk.sh
+++ upload_apk.sh
@@ -8,16 +8,16 @@ HOCKEYAPP_API_URL="https://rink.hockeyapp.net/api/2/apps/upload";
 
 token=$2;
 commit_id=$(git rev-parse HEAD);
-apk_name=$(ls *$1*.apk);
+apk_path=$1;
 
-echo "Uploading $apk_name...BEGIN"
+echo "Uploading $apk_path...BEGIN"
 
 curl -F "status=$AVAILABLE_TO_DOWNLOAD" \
      -F "notify=$NOTIFY_ALL_TESTERS" \
      -F "notes_type=$TEXT_TYPE" \
      -F "note=$commit_id" \
-     -F "ipa=@$apk_name" \
+     -F "ipa=@$apk_path" \
      -H "X-HockeyAppToken: $token" \
      $HOCKEYAPP_API_URL
 
-echo "Uploading $apk_name...DONE"
+echo "Uploading $apk_path...DONE"
