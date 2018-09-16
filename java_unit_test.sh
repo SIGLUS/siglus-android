e37aa0527947f5fbd2fbec3a81d669bd781b3a98
JingZhao <jingzhao@thoughtworks.com>
2018-09-16 14:29:43 +0800

    let dockerfile && test agent to master

--- java_unit_test.sh
+++ java_unit_test.sh
@@ -2,7 +2,7 @@
 set -e
 
 taskrunner="${TASKRUNNER:-siglus/test-runner:1.0.0}"
-pipelinename="${PIPELINE_NAME:-mobile_test_build}"
+pipelinename="${PIPELINE_NAME:-mobile_build}"
 dockernet="${DOCKER_NET:-gocd_default}"
 gradleexec="./gradlew clean testLocalDebug"
 LOCAL_DOCKER_SCRIPT="docker run --rm -v $(pwd):/openlmis-mobile -v gradle_cache:/root/.gradle --name test_local_debug -e JAVA_TOOL_OPTIONS=\"-Xmx4096m -XX:MaxPermSize=512m -Xms128m\" -e JAVA_OPTS=\"-XX:MaxPermSize=512m\" --memory 4096m -w /openlmis-mobile $taskrunner $gradleexec"
