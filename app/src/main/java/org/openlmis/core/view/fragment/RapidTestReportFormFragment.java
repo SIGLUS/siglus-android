a5482f312740b98a890eaad03a3205758862ea3f
JingZhao <jingzhao@thoughtworks.com>
2018-10-17 08:53:39 +0800

    [63] reformate code

--- app/src/main/java/org/openlmis/core/view/fragment/RapidTestReportFormFragment.java
+++ app/src/main/java/org/openlmis/core/view/fragment/RapidTestReportFormFragment.java
@@ -163,7 +163,7 @@ public class RapidTestReportFormFragment extends BaseReportFragment {
                     errorMessage = getString(R.string.error_rapid_test_unjustified);
                 } else if (!presenter.getViewModel().validateAPES()) {
                     errorMessage = getString(R.string.error_rapid_test_ape);
-                }else if (presenter.getViewModel().validateOnlyAPES()) {
+                } else if (presenter.getViewModel().validateOnlyAPES()) {
                     errorMessage = getString(R.string.error_rapid_test_only_ape);
                 }
 
