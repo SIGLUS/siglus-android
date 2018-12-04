12eaed5c178cd1e0585fd11522953f4df3dd02f6
JingZhao <jingzhao@thoughtworks.com>
2018-12-04 13:57:41 +0800

    [67] add service model

---
+++ app/src/main/java/org/openlmis/core/model/Service.java
@@ -0,0 +1,46 @@
+/*
+ * This program is part of the OpenLMIS logistics management information
+ * system platform software.
+ *
+ * Copyright © 2015 ThoughtWorks, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version. This program is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details. You should
+ * have received a copy of the GNU Affero General Public License along with
+ * this program. If not, see http://www.gnu.org/licenses. For additional
+ * information contact info@OpenLMIS.org
+ */
+
+package org.openlmis.core.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@DatabaseTable(tableName = "Services")
+public class Service extends BaseModel {
+
+    @Expose
+    @SerializedName("name")
+    @DatabaseField
+    private String name;
+
+    @Expose
+    @SerializedName("code")
+    @DatabaseField
+    private String code;
+
+    @DatabaseField
+    private boolean active;
+}
