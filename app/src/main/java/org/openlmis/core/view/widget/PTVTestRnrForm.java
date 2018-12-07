71a04a31f4c5748def2b6d3b3b50c0940dc6b2c9
JingZhao <jingzhao@thoughtworks.com>
2018-12-07 17:05:12 +0800

    [67] init PTV Test list form

---
+++ app/src/main/java/org/openlmis/core/view/widget/PTVTestRnrForm.java
@@ -0,0 +1,16 @@
+package org.openlmis.core.view.widget;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.widget.LinearLayout;
+
+public class PTVTestRnrForm extends LinearLayout{
+
+    private LayoutInflater layoutInflater;
+
+    public PTVTestRnrForm(Context context) {
+        super(context);
+        layoutInflater = LayoutInflater.from(context);
+
+    }
+}
