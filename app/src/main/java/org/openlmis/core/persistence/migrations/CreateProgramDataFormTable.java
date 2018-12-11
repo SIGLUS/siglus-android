4b58cde539ab7aceeb64e11507565337d87b6abb
JingZhao <jingzhao@thoughtworks.com>
2018-12-11 18:04:28 +0800

    modify observation in database

--- app/src/main/java/org/openlmis/core/persistence/migrations/CreateProgramDataFormTable.java
+++ app/src/main/java/org/openlmis/core/persistence/migrations/CreateProgramDataFormTable.java
@@ -9,7 +9,7 @@ public class CreateProgramDataFormTable extends Migration {
                 "(`submittedTime` VARCHAR , " +
                 "`periodBegin` VARCHAR , " +
                 "`periodEnd` VARCHAR , " +
-                "`observataion` VARCHAR DEFAULT '' , " +
+                "`observation` VARCHAR DEFAULT '' , " +
                 "`program_id` BIGINT , " +
                 "`status` VARCHAR DEFAULT 'DRAFT' , " +
                 "`synced` SMALLINT , " +
