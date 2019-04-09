f636b0b748a8d9c53a1177a87ca488e40167b0b4
JingZhao <jingzhao@thoughtworks.com>
2019-04-09 22:06:30 +0800

    Migration Fix: update rapid test program code

--- app/src/main/java/org/openlmis/core/persistence/LmisSqliteOpenHelper.java
+++ app/src/main/java/org/openlmis/core/persistence/LmisSqliteOpenHelper.java
@@ -87,6 +87,7 @@ import org.openlmis.core.persistence.migrations.UpdateCreateTimeAndUpdateTime;
 import org.openlmis.core.persistence.migrations.UpdateCustomRegimes;
 import org.openlmis.core.persistence.migrations.UpdateProductsFalseValueToZero;
 import org.openlmis.core.persistence.migrations.UpdateProgramDataFormTable;
+import org.openlmis.core.persistence.migrations.UpdateRapidTestCode;
 import org.openlmis.core.persistence.migrations.UpdateStockCardProductType;
 import org.openlmis.core.persistence.migrations.UpdateRegimenType;
 
@@ -161,6 +162,7 @@ public final class LmisSqliteOpenHelper extends OrmLiteSqliteOpenHelper {
             add(new AddCustomAmountAndTotalToRnrFormTable());
             add(new AddServiceItemTable());
             add(new AddProgramToRegimen());
+            add(new UpdateRapidTestCode());
         }
     };
     private static int instanceCount = 0;
---
+++ app/src/main/java/org/openlmis/core/persistence/migrations/UpdateRapidTestCode.java
@@ -0,0 +1,14 @@
+package org.openlmis.core.persistence.migrations;
+
+import org.openlmis.core.persistence.Migration;
+
+public class UpdateRapidTestCode extends Migration {
+    @Override
+    public void up() {
+        // 后端代码将rapidTest 迁移到 TEST_KIT
+        execSQL("UPDATE program_data_columns SET program_id = (SELECT id FROM programs WHERE programCode = 'TEST_KIT') "
+                + "WHERE program_id = (SELECT id FROM programs WHERE programCode = 'RAPID_TEST')");
+        execSQL("UPDATE program_data_forms SET program_id = (SELECT id FROM programs WHERE programCode = 'TEST_KIT') "
+                + "WHERE program_id = (SELECT id FROM programs WHERE programCode = 'RAPID_TEST')");
+    }
+}
