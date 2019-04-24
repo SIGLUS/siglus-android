6ab60a9a809e7978a133dd3421c60455e349a5f0
Peng.Cao <pcao@thoughtworks.com>
2019-04-24 18:34:05 +0800

    [capeng] fix we should have an “ X” button to remove it , adjust testcase

--- app/src/test/java/org/openlmis/core/view/holder/RequisitionProductViewHolderTest.java
+++ app/src/test/java/org/openlmis/core/view/holder/RequisitionProductViewHolderTest.java
@@ -89,7 +89,7 @@ public class RequisitionProductViewHolderTest {
         RequisitionFormItemViewModel viewModel = new RequisitionFormItemViewModel(formItem);
         viewHolder.populate(viewModel, presenter, LMISTestApp.getContext());
 
-        assertThat(viewHolder.ivDelete.getVisibility()).isEqualTo(View.VISIBLE);
+        assertThat(viewHolder.ivDelete.getVisibility()).isEqualTo(View.INVISIBLE);
     }
 
     @Test
