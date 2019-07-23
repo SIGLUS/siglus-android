067cf6a94d554dd1b84bad9884b2e0b8dc4e66a0
Peng.Cao <pcao@thoughtworks.com>
2019-07-23 15:51:57 +0800

    mmia view restore

--- app/src/main/java/org/openlmis/core/view/holder/RapidTestReportRowViewHolder.java
+++ app/src/main/java/org/openlmis/core/view/holder/RapidTestReportRowViewHolder.java
@@ -2,9 +2,7 @@ package org.openlmis.core.view.holder;
 
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.View;
-import android.widget.TextView;
 
 import org.openlmis.core.LMISApp;
 import org.openlmis.core.R;
@@ -40,16 +38,6 @@ public class RapidTestReportRowViewHolder extends BaseViewHolder {
                 false));
         rvRapidReportGridListView.setAdapter(adapter);
     }
-
-//    public void setUpHeader(RapidTestFormItemViewModel viewModel) {
-//        Log.e(TAG,"caopeng setUpHeader ="+viewModel.getIssueReason().getDescription());
-////        tvRowHeader.setText(viewModel.getIssueReason().getDescription());
-////        if (isTotal(viewModel) || isAPEs(viewModel)) {
-////            tvRowHeader.setBackgroundColor(LMISApp.getInstance().getResources().getColor(R.color.color_rapid_test_form_total_header));
-////        } else {
-////            tvRowHeader.setBackgroundColor(LMISApp.getInstance().getResources().getColor(R.color.color_rapid_test_form_row_header));
-////        }
-//    }
 
     public boolean isTotal(RapidTestFormItemViewModel viewModel) {
         return viewModel.getIssueReason().getDescription().equals(LMISApp.getInstance().getString(R.string.total));
