a07461177dc16ff69c76b11afccb5835f38f9213
Peng.Cao <pcao@thoughtworks.com>
2020-04-20 14:48:21 +0800

    [Trello No.8]:The Freeze panel of rapid test  will auto scroll to right when input

---
+++ app/src/main/java/org/openlmis/core/view/widget/RapidTestBodyHorizontalScrollView.java
@@ -0,0 +1,26 @@
+package org.openlmis.core.view.widget;
+
+import android.content.Context;
+import android.graphics.Rect;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.HorizontalScrollView;
+
+public class RapidTestBodyHorizontalScrollView extends HorizontalScrollView {
+    public RapidTestBodyHorizontalScrollView(Context context) {
+        super(context);
+    }
+
+    public RapidTestBodyHorizontalScrollView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public RapidTestBodyHorizontalScrollView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    public boolean requestChildRectangleOnScreen(View child, Rect rectangle, boolean immediate) {
+        return true;
+    }
+}
