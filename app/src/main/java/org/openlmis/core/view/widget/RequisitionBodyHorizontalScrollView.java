414e3ce82ec24bf0ffbca343f46cf1562e6bacc2
Peng.Cao <pcao@thoughtworks.com>
2020-07-01 16:29:53 +0800

    [Trello No.185]Freezy panel not working fine

---
+++ app/src/main/java/org/openlmis/core/view/widget/RequisitionBodyHorizontalScrollView.java
@@ -0,0 +1,31 @@
+package org.openlmis.core.view.widget;
+
+import android.content.Context;
+import android.graphics.Rect;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.HorizontalScrollView;
+
+public class RequisitionBodyHorizontalScrollView extends HorizontalScrollView {
+    public RequisitionBodyHorizontalScrollView(Context context) {
+        super(context);
+    }
+
+    public RequisitionBodyHorizontalScrollView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public RequisitionBodyHorizontalScrollView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    public boolean requestChildRectangleOnScreen(View child, Rect rectangle, boolean immediate) {
+        return true;
+    }
+
+    @Override
+    protected int computeScrollDeltaToGetChildRectOnScreen(Rect rect) {
+        return 0;
+    }
+}
