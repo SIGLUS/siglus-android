4f68f4574ac65a4677116c5a2c9bef8e412018b6
Boyu Yuan <boyu.yuan@thoughtworks.com>
2021-05-27 12:50:37 +0800

    [boyu yuan] No.333 when the user has not submitted reprot for more than one year,let him submit from 12 months ago

--- app/src/main/java/org/openlmis/core/model/service/ProgramDataFormPeriodService.java
+++ app/src/main/java/org/openlmis/core/model/service/ProgramDataFormPeriodService.java
@@ -2,27 +2,63 @@ package org.openlmis.core.model.service;
 
 import com.google.inject.Inject;
 
+import org.joda.time.DateTime;
+import org.joda.time.Months;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 import org.openlmis.core.exceptions.LMISException;
 import org.openlmis.core.model.Period;
+import org.openlmis.core.model.ProgramDataForm;
+import org.openlmis.core.model.ReportTypeForm;
 import org.openlmis.core.model.StockMovementItem;
+import org.openlmis.core.model.repository.ProgramDataFormRepository;
+import org.openlmis.core.model.repository.ReportTypeFormRepository;
 import org.openlmis.core.model.repository.StockMovementRepository;
-import org.openlmis.core.model.repository.StockRepository;
+import org.openlmis.core.utils.Constants;
 import org.roboguice.shaded.goole.common.base.Optional;
+
+import java.util.List;
 
 public class ProgramDataFormPeriodService {
     @Inject
-    StockRepository stockRepository;
+    private ReportTypeFormRepository reportTypeFormRepository;
+
+    @Inject
+    private ProgramDataFormRepository programDataFormRepository;
+
     @Inject
     private StockMovementRepository stockMovementRepository;
 
     public Optional<Period> getFirstStandardPeriod() throws LMISException {
-        StockMovementItem firstStockMovement = stockMovementRepository.getFirstStockMovement();
-        if (firstStockMovement != null) {
-            Period firstPeriod = firstStockMovement.getMovementPeriod();
+        DateTime initializeDateTime;
+        Period period = null;
+        DateTimeFormatter dateTimeFormatter = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss");
+        ReportTypeForm reportTypeForm = reportTypeFormRepository.queryByCode(Constants.TEST_KIT_PROGRAM_CODE);
 
-            if (firstPeriod != null && firstPeriod.isOpenToRequisitions()) {
-                return Optional.of(firstPeriod);
+        if (reportTypeForm.lastReportEndTime != null) {
+            DateTime lastReportEndTime = dateTimeFormatter.parseDateTime(reportTypeForm.lastReportEndTime);
+            if (Months.monthsBetween(lastReportEndTime, new DateTime()).getMonths() > 12) {
+                initializeDateTime = new DateTime().plusMonths(-12).toDateTime();
+                period = new Period(initializeDateTime);
+            } else {
+                List<ProgramDataForm> forms = programDataFormRepository.listByProgramCode(Constants.TEST_KIT_PROGRAM_CODE);
+                if (forms != null) {
+                    period = new Period(new DateTime(forms.get(0).getPeriodBegin()));
+                } else {
+                    StockMovementItem firstStockMovement = stockMovementRepository.getFirstStockMovement();
+                    if (firstStockMovement != null) {
+                        period = firstStockMovement.getMovementPeriod();
+                    }
+                }
             }
+        } else {
+            StockMovementItem firstStockMovement = stockMovementRepository.getFirstStockMovement();
+            if (firstStockMovement != null) {
+                period = firstStockMovement.getMovementPeriod();
+            }
+        }
+        if (period != null && period.isOpenToRequisitions()) {
+            return Optional.of(period);
         }
         return Optional.absent();
     }
