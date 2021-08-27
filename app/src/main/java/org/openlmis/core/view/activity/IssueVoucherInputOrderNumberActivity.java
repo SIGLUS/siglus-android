69869a048c2a35c7c88d99555cd5ef2bbf633f6f
Boyu Yuan <boyu.yuan@thoughtworks.com>
2021-08-27 10:06:39 +0800

    [boyu yuan] #N/A fix background click logic

--- app/src/main/java/org/openlmis/core/view/activity/IssueVoucherInputOrderNumberActivity.java
+++ app/src/main/java/org/openlmis/core/view/activity/IssueVoucherInputOrderNumberActivity.java
@@ -30,6 +30,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import androidx.activity.result.ActivityResultLauncher;
@@ -261,16 +262,13 @@ public class IssueVoucherInputOrderNumberActivity extends BaseActivity {
   }
 
   @NonNull
-  private SingleClickButtonListener getBackgroundClickListener() {
-    return new SingleClickButtonListener() {
-      @Override
-      public void onSingleClick(View v) {
-        if (orderNumber != null) {
-          validateOrderNumber();
-        }
-        etOrderNumber.clearFocus();
-        hideKeyboard(v);
+  private OnClickListener getBackgroundClickListener() {
+    return v -> {
+      if (orderNumber != null) {
+        validateOrderNumber();
       }
+      etOrderNumber.clearFocus();
+      hideKeyboard(v);
     };
   }
 
