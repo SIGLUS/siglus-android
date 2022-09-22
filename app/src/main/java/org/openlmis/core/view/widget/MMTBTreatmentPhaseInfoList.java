44d43fd1c52cdc5b6d7c813a85bef123c1b7fa73
xuzhenhui <zhenhui.xu@thoughtworks.com>
2022-09-22 14:35:28 +0800

    [zhenhui] #303 feat: set mmtb treatment phase form ui and mock data

---
+++ app/src/main/java/org/openlmis/core/view/widget/MMTBTreatmentPhaseInfoList.java
@@ -0,0 +1,163 @@
+/*
+ * This program is part of the OpenLMIS logistics management information
+ * system platform software.
+ *
+ * Copyright © 2015 ThoughtWorks, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version. This program is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details. You should
+ * have received a copy of the GNU Affero General Public License along with
+ * this program. If not, see http://www.gnu.org/licenses. For additional
+ * information contact info@OpenLMIS.org
+ */
+
+package org.openlmis.core.view.widget;
+
+import android.content.Context;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.openlmis.core.R;
+
+public class MMTBTreatmentPhaseInfoList extends LinearLayout {
+
+  private static final String MOCK_ADULT_CATEGORY = "Adult";
+  private static final String MOCK_PEDIATRIC_CATEGORY = "Pediatric";
+
+  private static final List<TreatmentPhaseItem> mockData = Collections.unmodifiableList(Arrays.asList(
+      new TreatmentPhaseItem("Adult Treatment Phase 1", "", MOCK_ADULT_CATEGORY),
+      new TreatmentPhaseItem("Adult Treatment Phase 2", "", MOCK_ADULT_CATEGORY),
+      new TreatmentPhaseItem("Adult Treatment Phase 3", "", MOCK_ADULT_CATEGORY),
+      new TreatmentPhaseItem("Adult Treatment Phase 4", "", MOCK_ADULT_CATEGORY),
+      new TreatmentPhaseItem("Adult Treatment Phase 5", "", MOCK_ADULT_CATEGORY),
+      new TreatmentPhaseItem("Adult Treatment Phase 6", "", MOCK_ADULT_CATEGORY),
+      new TreatmentPhaseItem("Adult Treatment Phase 7", "", MOCK_ADULT_CATEGORY),
+      new TreatmentPhaseItem("Pediatric Treatment Phase 1", "", MOCK_PEDIATRIC_CATEGORY),
+      new TreatmentPhaseItem("Pediatric Treatment Phase 2", "", MOCK_PEDIATRIC_CATEGORY),
+      new TreatmentPhaseItem("Pediatric Treatment Phase 3", "", MOCK_PEDIATRIC_CATEGORY),
+      new TreatmentPhaseItem("Pediatric Treatment Phase 4", "", MOCK_PEDIATRIC_CATEGORY),
+      new TreatmentPhaseItem("Pediatric Treatment Phase 5", "", MOCK_PEDIATRIC_CATEGORY),
+      new TreatmentPhaseItem("Pediatric Treatment Phase 6", "", MOCK_PEDIATRIC_CATEGORY),
+      new TreatmentPhaseItem("Pediatric Treatment Phase 7", "", MOCK_PEDIATRIC_CATEGORY)
+  ));
+
+  private RotateTextView rtvAdult;
+  private RotateTextView rtvPediatric;
+  private final LinearLayout llAdultContainer;
+  private final LinearLayout llPediatricContainer;
+  private final LayoutInflater layoutInflater;
+  private final List<EditText> editTexts = new ArrayList<>();
+
+  public MMTBTreatmentPhaseInfoList(Context context) {
+    this(context, null);
+  }
+
+  public MMTBTreatmentPhaseInfoList(Context context, @Nullable AttributeSet attrs) {
+    this(context, attrs, 0);
+  }
+
+  public MMTBTreatmentPhaseInfoList(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+    super(context, attrs, defStyleAttr);
+    layoutInflater = LayoutInflater.from(context);
+    layoutInflater.inflate(R.layout.layout_mmtb_requisition_treatment_phase_info, this, true);
+    llAdultContainer = findViewById(R.id.ll_adult_container);
+    llPediatricContainer = findViewById(R.id.ll_pediatric_container);
+    rtvAdult = findViewById(R.id.rtv_adult);
+    rtvPediatric = findViewById(R.id.rtv_pediatric);
+    setData();
+  }
+
+  public boolean isCompleted() {
+    for (EditText editText : editTexts) {
+      if (TextUtils.isEmpty(editText.getText().toString())) {
+        editText.setError(getContext().getString(R.string.hint_error_input));
+        editText.requestFocus();
+        return false;
+      }
+    }
+    return true;
+  }
+
+  // TODO set data from form
+  private void setData() {
+    for (TreatmentPhaseItem phaseItem : mockData) {
+      View itemView = layoutInflater.inflate(R.layout.item_mmtb_requisition_treatment_phase, this, false);
+      TextView tvTitle = itemView.findViewById(R.id.tv_title);
+      tvTitle.setText(phaseItem.name);
+      EditText etAmount = itemView.findViewById(R.id.et_treatment_phase_amount);
+      etAmount.setText(phaseItem.value);
+      etAmount.addTextChangedListener(new EditTextWatcher(phaseItem));
+      editTexts.add(etAmount);
+      if (MOCK_ADULT_CATEGORY.equals(phaseItem.category)) {
+        itemView.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.color_cad9ee));
+        etAmount.setBackground(ContextCompat.getDrawable(getContext(), R.drawable.bg_blue_no_border));
+        llAdultContainer.addView(itemView);
+      } else {
+        itemView.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.color_96b5dd));
+        etAmount.setBackground(ContextCompat.getDrawable(getContext(), R.drawable.bg_light_blue_no_border));
+        llPediatricContainer.addView(itemView);
+      }
+    }
+    post(this::updateLeftHeader);
+  }
+
+  private void updateLeftHeader() {
+    LayoutParams adultParams = (LayoutParams) rtvAdult.getLayoutParams();
+    adultParams.height = llAdultContainer.getHeight();
+    rtvAdult.setLayoutParams(adultParams);
+    LayoutParams childrenParams = (LayoutParams) rtvPediatric.getLayoutParams();
+    childrenParams.height = llPediatricContainer.getHeight();
+    rtvPediatric.setLayoutParams(childrenParams);
+  }
+
+  @AllArgsConstructor
+  @Data
+  private static class TreatmentPhaseItem {
+
+    private String name;
+    private String value;
+    private String category;
+  }
+
+  private static class EditTextWatcher implements android.text.TextWatcher {
+
+    private final TreatmentPhaseItem item;
+
+    public EditTextWatcher(TreatmentPhaseItem item) {
+      this.item = item;
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+      // do nothing
+    }
+
+    @Override
+    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+      // do nothing
+    }
+
+    @Override
+    public void afterTextChanged(Editable editable) {
+      item.setValue(editable.toString());
+    }
+  }
+}
--- app/src/main/java/org/openlmis/core/view/widget/RotateTextView.java
+++ app/src/main/java/org/openlmis/core/view/widget/RotateTextView.java
@@ -20,33 +20,20 @@ package org.openlmis.core.view.widget;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Rect;
 import android.util.AttributeSet;
-import android.view.Gravity;
 import androidx.appcompat.widget.AppCompatTextView;
 
 public class RotateTextView extends AppCompatTextView {
 
-  private boolean topDown;
+  private final Rect bounds = new Rect();
 
   public RotateTextView(Context context, AttributeSet attrs) {
     super(context, attrs);
-    init();
-  }
-
-  private void init() {
-    final int gravity = getGravity();
-    if (Gravity.isVertical(gravity)
-        && (gravity & Gravity.VERTICAL_GRAVITY_MASK) == Gravity.BOTTOM) {
-      setGravity((gravity & Gravity.HORIZONTAL_GRAVITY_MASK) | Gravity.TOP);
-      topDown = false;
-    } else {
-      topDown = true;
-    }
   }
 
   public RotateTextView(Context context) {
     super(context);
-    init();
   }
 
   @Override
@@ -59,18 +46,12 @@ public class RotateTextView extends AppCompatTextView {
   protected void onDraw(Canvas canvas) {
     getPaint().setColor(getCurrentTextColor());
     getPaint().drawableState = getDrawableState();
-
+    String text = getText().toString();
+    getPaint().getTextBounds(text, 0, text.length(), bounds);
     canvas.save();
-
-    if (topDown) {
-      canvas.translate(getWidth(), getHeight() / 4f);
-      canvas.rotate(90);
-    } else {
-      canvas.translate(getWidth() / 4f, getHeight());
-      canvas.rotate(-90);
-    }
-    canvas.translate(getCompoundPaddingLeft(), getExtendedPaddingTop());
-
+    canvas.translate(getWidth() / 2f - bounds.height() - getTotalPaddingStart(), getHeight());
+    canvas.rotate(-90);
+    canvas.translate(getTotalPaddingLeft(), getExtendedPaddingTop());
     getLayout().draw(canvas);
     canvas.restore();
   }
