3aeef447087d911469de07e16170bfb04ad3c957
luoyang <luoyang.gao@thoughtworks.com>
2022-09-27 11:09:43 +0800

    [luoyang] fix: remove unused class for checkstyle

--- app/src/main/java/org/openlmis/core/view/widget/MMIARegimeThreeLineList.java
+++ app/src/main/java/org/openlmis/core/view/widget/MMIARegimeThreeLineList.java
@@ -26,7 +26,6 @@ import android.util.AttributeSet;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
