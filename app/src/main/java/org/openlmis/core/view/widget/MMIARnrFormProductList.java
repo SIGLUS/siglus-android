e56dd8b0479a8afc66941395ab41f9596b03a477
jun-wu-tw <jun.wu@thoughtworks.com>
2024-07-26 16:24:52 +0800

    [Jun Wu] #N/A fix: can't submit `MMIA` requisition

--- app/src/main/java/org/openlmis/core/view/widget/MMIARnrFormProductList.java
+++ app/src/main/java/org/openlmis/core/view/widget/MMIARnrFormProductList.java
@@ -33,6 +33,7 @@ import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import androidx.annotation.Nullable;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.Getter;
@@ -292,9 +293,20 @@ public class MMIARnrFormProductList extends LinearLayout {
       boolean isArchived = item.getProduct().isArchived();
       tvInitialAmount.setText(getValue(isArchived, item.getInitialAmount()));
       tvReceived.setText(getValue(isArchived, item.getReceived()));
-      editTexts.add(configEditText(item, etIssued, getValue(isArchived, item.getIssued())));
-      editTexts.add(configEditText(item, etAdjustment, getValue(isArchived, item.getAdjustment())));
-      editTexts.add(configEditText(item, etInventory, getValue(isArchived, item.getInventory())));
+
+      String issuedValue = getValue(isArchived, item.getIssued());
+      String adjustmentValue = getValue(isArchived, item.getAdjustment());
+      String inventoryValue = getValue(isArchived, item.getInventory());
+      // should initialize ViewModel value if the product is archived
+      if (isArchived) {
+        item.setIssued(convertLongFromLongString(issuedValue));
+        item.setAdjustment(convertLongFromLongString(adjustmentValue));
+        item.setInventory(convertLongFromLongString(inventoryValue));
+      }
+
+      editTexts.add(configEditText(item, etIssued, issuedValue));
+      editTexts.add(configEditText(item, etAdjustment, adjustmentValue));
+      editTexts.add(configEditText(item, etInventory, inventoryValue));
       rightViewGroup.addView(inflate);
 
       try {
@@ -408,14 +420,19 @@ public class MMIARnrFormProductList extends LinearLayout {
     }
 
     private Long getEditValue(Editable etText) {
-      Long editTextValue;
-      try {
-        editTextValue = Long.valueOf(etText.toString());
-      } catch (NumberFormatException e) {
-        editTextValue = null;
-      }
-      return editTextValue;
+      return convertLongFromLongString(etText.toString());
     }
+  }
+
+  @Nullable
+  private Long convertLongFromLongString(String longString) {
+    Long editTextValue;
+    try {
+      editTextValue = Long.valueOf(longString);
+    } catch (NumberFormatException e) {
+      editTextValue = null;
+    }
+    return editTextValue;
   }
 
   public void removeOriginalTable() {
