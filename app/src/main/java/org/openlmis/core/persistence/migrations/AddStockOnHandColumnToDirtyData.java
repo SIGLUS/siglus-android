5dad0529bdd4a35ea2621866070daced96aff7af
luoyang <luoyang.gao@thoughtworks.com>
2024-12-05 19:55:21 +0800

    [luoyang] #692 fix: app crash when update from 2.0.5 to 2.1.0

--- app/src/main/java/org/openlmis/core/persistence/migrations/AddStockOnHandColumnToDirtyData.java
+++ app/src/main/java/org/openlmis/core/persistence/migrations/AddStockOnHandColumnToDirtyData.java
@@ -1,11 +1,33 @@
 package org.openlmis.core.persistence.migrations;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import org.openlmis.core.persistence.Migration;
 
 public class AddStockOnHandColumnToDirtyData extends Migration {
 
   @Override
   public void up() {
-    execSQL("ALTER TABLE 'dirty_data' ADD COLUMN stockOnHand BIGINT");
+    if (!isColumnExists(this.db, "dirty_data", "stockOnHand")) {
+      execSQL("ALTER TABLE 'dirty_data' ADD COLUMN stockOnHand BIGINT");
+    }
+  }
+
+  private boolean isColumnExists(SQLiteDatabase db, String tableName, String columnName) {
+    Cursor cursor = null;
+    try {
+      cursor = db.rawQuery("PRAGMA table_info(" + tableName + ")", null);
+      while (cursor.moveToNext()) {
+        String name = cursor.getString(cursor.getColumnIndexOrThrow("name"));
+        if (name.equals(columnName)) {
+          return true;
+        }
+      }
+    } finally {
+      if (cursor != null) {
+        cursor.close();
+      }
+    }
+    return false;
   }
 }
